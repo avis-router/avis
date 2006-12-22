2d9624475754c24b2fdf36fc79431d8ba94202d3
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2006-12-22 08:58:26 +0000

    Added -daemon and -pidfile options.

--- trunk/bin/avisd.sh
+++ trunk/bin/avisd.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+set -e
+
+daemon=0
+avisd_opts=""
+logfile="/dev/null" 
 avisd_jar=`dirname $0`/../lib/avisd.jar
 
 if [ ! -e $avisd_jar ]; then
@@ -7,4 +12,29 @@ if [ ! -e $avisd_jar ]; then
   exit 1
 fi
 
-java -server -jar $avisd_jar $*
+while [ $# -gt 0 ]; do
+  case $1 in
+    -pidfile) pidfile=$2; shift 2;;
+    -daemon)  daemon=1; shift;;
+    -logfile) logfile=$2; shift 2;;
+    *)        avisd_opts="$avisd_opts $1"; shift;;
+  esac
+done
+
+command="java -server -jar $avisd_jar $avisd_opts"
+
+if [ $daemon == 1 ]; then
+  (
+    exec $command < /dev/null 2>&1 > $logfile
+  ) &
+  
+  if [ "x$pidfile" != "x" ]; then
+    echo $! > "$pidfile"
+  fi
+else
+  if [ "x$pidfile" != "x" ]; then
+    echo $$ > "$pidfile"
+  fi
+  
+  exec $command
+fi
