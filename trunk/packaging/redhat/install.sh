6e232295cf419f9956912f93a307957c61879edb
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2006-12-24 06:23:48 +0000

    Added permission settings.

--- trunk/packaging/redhat/install.sh
+++ trunk/packaging/redhat/install.sh
@@ -3,7 +3,19 @@
 home=$(dirname $0)
 prefix=/
 
-OPTS=`getopt -o p: --long prefix: -n '$0' -- "$@"`
+usage ()
+{
+  local NL=$'\x0a'
+  local help="\
+  Usage: $0 [-h|--help] [-p|--prefix dir] $NL\
+
+     -h|--help      : This text$NL\
+     -p--prefix dir : Set install dir prefix (default is \"/\")"
+ 
+  echo "$help" >&2
+}
+
+OPTS=`getopt -a -o p:h --long prefix:,help -n '$0' -- "$@"`
 
 if [ $? != 0 ] ; then exit 1 ; fi
 
@@ -11,11 +23,15 @@ eval set -- "$OPTS"
 
 while [ $# -gt 0 ]; do
   case "$1" in
-    --prefix) prefix=$2 ; shift 2 ;;
+    --prefix|-p) prefix=$2 ; shift 2 ;;
+    -h|--help) usage ; exit 0 ;;
     --) shift ; break ;;
     *) echo "!error" ; shift 1 ;;
   esac
 done
 
+chmod -R 0644 $home && \
+chmod 0755 $home/usr/local/sbin/avisd && \
+chmod 0755 $home/etc/init.d/avisd && \
 chown -R root:root $home/root && \
-cp -rp $home/root/* $prefix
+cp -rpv $home/root/* $prefix
