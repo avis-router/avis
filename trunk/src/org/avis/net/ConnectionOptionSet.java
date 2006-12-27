cb19c6a6c58342e80db7e563f3d0e12cbb9faee6
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2006-12-27 08:24:10 +0000

    Extended ranges for Connection.Max-Keys and Subscription.Max-Keys.

--- trunk/src/org/avis/net/ConnectionOptionSet.java
+++ trunk/src/org/avis/net/ConnectionOptionSet.java
@@ -127,8 +127,8 @@ public class ConnectionOptionSet extends OptionSet
     
     // Avis-specific
     // Max connection keys for ntfn/sub
-    add ("Connection.Max-Keys", 1*K, 1*K, 1*K);
-    add ("Subscription.Max-Keys", 1*K, 1*K, 1*K);
+    add ("Connection.Max-Keys", 8, 1*K, 1*K);
+    add ("Subscription.Max-Keys", 8, 1*K, 1*K);
     
     // ------------ Legacy options
     
