e1dce4b7a10f42b69a7b9ef09550a46ca5b09a12
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-01-22 06:23:11 +0000

    Added map ().

--- trunk/src/org/avis/util/Collections.java
+++ trunk/src/org/avis/util/Collections.java
@@ -2,8 +2,10 @@ package org.avis.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -27,5 +29,22 @@ public class Collections
   public static List<String> list (String... strings)
   {
     return new ArrayList<String> (Arrays.asList (strings));
+  }
+  
+  /**
+   * Create a map from a number of strings, even items are keys,
+   * adjacent items are values.
+   */
+  public static Map<String, String> map (String... strings)
+  {
+    if (strings.length % 2 != 0)
+      throw new IllegalArgumentException ("Strings must be a set of pairs");
+    
+    HashMap<String, String> map = new HashMap<String, String> ();
+    
+    for (int i = 0; i < strings.length; i += 2)
+      map.put (strings [i], strings [i + 1]);
+
+    return map;
   }
 }
