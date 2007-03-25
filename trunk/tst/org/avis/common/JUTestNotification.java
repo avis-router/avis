f33a686872620272f0d72a67f308891a95e2d3d5
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-03-25 03:37:22 +0000

    Added parsing tests.

--- trunk/tst/org/avis/common/JUTestNotification.java
+++ trunk/tst/org/avis/common/JUTestNotification.java
@@ -1,8 +1,14 @@
 package org.avis.common;
 
+import java.util.Arrays;
+
+import org.avis.util.InvalidFormatException;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class JUTestNotification
 {
@@ -33,5 +39,87 @@ public class JUTestNotification
        "opaque: [01 02 03 de ad be ef]\n" + 
        "int64: 4242L\n" + 
        "field\\ with\\ tricky:characters: \"bad\"", toString);
+  }
+  
+  @Test
+  public void parseSimple ()
+    throws Exception
+  {
+    String ntfnStr =
+      "String: \"string\"\n" +
+      "Int32: 42\n" +
+      "Int64: 24L\n" +
+      "Real64: 3.1415\n" +
+      "Opaque: [01 02 0a 0b 0c 1a ab ff]";
+    
+    Notification ntfn = new Notification (ntfnStr);
+    
+    assertEquals ("string", ntfn.get ("String"));
+    assertEquals (42, ntfn.get ("Int32"));
+    assertEquals (24L, ntfn.get ("Int64"));
+    assertEquals (3.1415, ntfn.get ("Real64"));
+    assertTrue
+      ("Opaque arrays equal",
+       Arrays.equals
+        (new byte [] {0x01, 0x02, 0x0a, 0x0b, 0x0c, 0x1a, (byte)0xab, (byte)0xff},
+         ntfn.getOpaque ("Opaque")));
+  }
+  
+  @Test
+  public void parseTricky ()
+    throws Exception
+  {
+    String ntfnStr =
+      "Str\\ ing: \"str\\\"ing\"\n" +
+      "Int\\:32: 42\n" +
+      "Int64:24L\n" +
+      "Real64 : 3.1415\n" +
+      "Opaque:[ ]";
+    
+    Notification ntfn = new Notification (ntfnStr);
+    
+    assertEquals ("str\"ing", ntfn.get ("Str ing"));
+    assertEquals (42, ntfn.get ("Int:32"));
+    assertEquals (24L, ntfn.get ("Int64"));
+    assertEquals (3.1415, ntfn.get ("Real64"));
+    assertTrue
+      ("Opaque arrays equal",
+       Arrays.equals
+        (new byte [] {},
+         ntfn.getOpaque ("Opaque")));
+  }
+  
+  @Test
+  public void parseErrors ()
+    throws Exception
+  {
+    assertInvalid ("Hello");
+    assertInvalid ("hello: ");
+    assertInvalid ("he:llo: 1");
+    assertInvalid ("hello: 1.a");
+    assertInvalid ("hello: hello");
+    assertInvalid ("hello: \"hello");
+    assertInvalid ("hello: \"hello\"\"");
+    assertInvalid ("hello: [");
+    assertInvalid ("hello: []]");
+    assertInvalid ("hello: [01 ");
+    assertInvalid ("hello: [01 001]");
+    assertInvalid ("hello: [01 1h]");
+    assertInvalid ("hello: [01 1h]");
+    assertInvalid ("hello: 1234567890123");
+  }
+
+  private static void assertInvalid (String ntfnExpr)
+  {
+    try
+    {
+      new Notification (ntfnExpr);
+      
+      fail ("Failed to detect invalid expression: " + ntfnExpr);
+    } catch (InvalidFormatException ex)
+    {
+      // System.out.println (ex.getMessage ());
+      // ok
+    }
   }
 }
