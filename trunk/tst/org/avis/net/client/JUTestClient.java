8b0489b598e3a9a2816c462e5dd36654bb1a4321
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-04-01 02:52:12 +0000

    Initial revision.

---
+++ trunk/tst/org/avis/net/client/JUTestClient.java
@@ -0,0 +1,71 @@
+package org.avis.net.client;
+
+import java.io.IOException;
+
+import org.avis.common.Notification;
+import org.avis.net.server.Server;
+
+import org.junit.Test;
+
+import static java.lang.System.currentTimeMillis;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class JUTestClient
+{
+  @Test
+  public void modifySubInNotifyEvent ()
+    throws Exception
+  {
+    Server server = new Server (29170);
+    
+    final Elvin client = new Elvin ("elvin://localhost:29170");
+    
+    final Subscription sub = client.subscribe ("require(test)");
+    
+    assertTrue (client.hasSubscription (sub));
+    
+    sub.addNotificationListener (new NotificationListener ()
+    {
+      public void notificationReceived (NotificationEvent e)
+      {
+        System.out.println ("notification!");
+        try
+        {
+          client.unsubscribe (sub);
+          
+          synchronized (sub)
+          {
+            sub.notifyAll ();
+          }
+        } catch (IOException ex)
+        {
+          throw new RuntimeException (ex);
+        }
+      }
+    });
+    
+    Notification ntfn = new Notification ();
+    ntfn.put ("test", 1);
+    ntfn.put ("payload", "test 1");
+    
+    client.send (ntfn);
+    
+    synchronized (sub)
+    {
+      long waitStart = currentTimeMillis ();
+      
+      sub.wait (5000);
+      
+      if (currentTimeMillis () - waitStart >= 5000)
+        fail ("Timed out waiting for unsubscribe");
+    }
+    
+    assertFalse (client.hasSubscription (sub));
+    
+    client.close ();
+    server.close ();
+  }
+}
