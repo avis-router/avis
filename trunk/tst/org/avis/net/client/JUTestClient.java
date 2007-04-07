f241e4e500254223c5afb9f2743ffb10d5fab061
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-04-07 10:01:25 +0000

    Added notifyInNotifyEvent () and security test.

--- trunk/tst/org/avis/net/client/JUTestClient.java
+++ trunk/tst/org/avis/net/client/JUTestClient.java
@@ -3,6 +3,10 @@ package org.avis.net.client;
 import java.io.IOException;
 
 import org.avis.common.Notification;
+import org.avis.net.common.ConnectionOptions;
+import org.avis.net.common.ElvinURI;
+import org.avis.net.security.Key;
+import org.avis.net.security.Keys;
 import org.avis.net.server.Server;
 import org.avis.util.LogFailTester;
 
@@ -10,6 +14,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.avis.net.security.KeyScheme.SHA1_PRODUCER;
+import static org.avis.net.security.Keys.EMPTY_KEYS;
 
 import static java.lang.System.currentTimeMillis;
 import static org.junit.Assert.assertFalse;
@@ -36,6 +43,9 @@ public class JUTestClient
     logTester.dispose ();
   }
   
+  /**
+   * Test that client handles changing subs in a notification callback.
+   */
   @Test
   public void modifySubInNotifyEvent ()
     throws Exception
@@ -44,7 +54,7 @@ public class JUTestClient
     
     final Elvin client = new Elvin ("elvin://localhost:29170");
     
-    final Subscription sub = client.subscribe ("require(test)");
+    final Subscription sub = client.subscribe ("require (test)");
     
     assertTrue (client.hasSubscription (sub));
     
@@ -52,6 +62,8 @@ public class JUTestClient
     {
       public void notificationReceived (NotificationEvent e)
       {
+        assertFalse (e.secure);
+        
         try
         {
           client.unsubscribe (sub);
@@ -71,22 +83,145 @@ public class JUTestClient
     ntfn.put ("test", 1);
     ntfn.put ("payload", "test 1");
     
-    
     synchronized (sub)
     {
       client.send (ntfn);
-      long waitStart = currentTimeMillis ();
       
-      sub.wait (10000);
-      
-      if (currentTimeMillis () - waitStart >= 10000)
-        fail ("Timed out waiting for unsubscribe");
+      wait (sub);
     }
     
     assertFalse (client.hasSubscription (sub));
     
     client.close ();
     server.close ();
+  }
+  
+  /**
+   * Test that client handles sending a notificaion in a notification
+   * callback.
+   */
+  @Test
+  public void notifyInNotifyEvent ()
+    throws Exception
+  {
+    server = new Server (29170);
+    
+    final Elvin client = new Elvin ("elvin://localhost:29170");
+    final Subscription sub = client.subscribe ("require (test)");
+    
+    sub.addNotificationListener (new NotificationListener ()
+    {
+      public void notificationReceived (NotificationEvent e)
+      {
+        assertFalse (e.secure);
+        
+        try
+        {
+          if (e.notification.get ("payload").equals ("test 1"))
+          {
+            Notification ntfn = new Notification ();
+            ntfn.put ("test", 1);
+            ntfn.put ("payload", "test 2");
+            
+            client.send (ntfn);
+          } else
+          {
+            synchronized (sub)
+            {
+              sub.notifyAll ();
+            }
+          }
+        } catch (IOException ex)
+        {
+          throw new RuntimeException (ex);
+        }
+      }
+    });
+    
+    Notification ntfn = new Notification ();
+    ntfn.put ("test", 1);
+    ntfn.put ("payload", "test 1");
+    
+    synchronized (sub)
+    {
+      client.send (ntfn);
+      
+      wait (sub);
+    }
+    
+    client.close ();
+    server.close ();
+  }
+
+  @Test
+  public void securityProducer ()
+    throws Exception
+  {
+    server = new Server (29170);
+    ElvinURI uri = new ElvinURI ("localhost", 29170);
+    ConnectionOptions options = new ConnectionOptions ();
+    
+    Key alicePrivate = new Key ("alice private");
+    Key alicePublic = alicePrivate.publicKeyFor (SHA1_PRODUCER);
+    
+    Keys aliceNtfnKeys = new Keys ();
+    aliceNtfnKeys.add (SHA1_PRODUCER, alicePrivate);
+    
+    Keys bobSubKeys = new Keys ();
+    bobSubKeys.add (SHA1_PRODUCER, alicePublic);
+    
+    Elvin aliceClient = new Elvin (uri, options, aliceNtfnKeys, EMPTY_KEYS);
+    Elvin bobClient = new Elvin (uri, options, EMPTY_KEYS, bobSubKeys);
+    
+    Subscription sub = bobClient.subscribe ("require (From-Alice)");
+    
+    checkSecureSendReceive (aliceClient, sub);
+    
+    aliceClient.close ();
+    bobClient.close ();
+    
+    // check we can add global keys later for same result
+    
+//    aliceClient = new Elvin (uri);
+//    bobClient = new Elvin (uri);
+//    
+//    aliceClient.setKeys (aliceNtfnKeys, EMPTY_KEYS);
+//    bobClient.setKeys (EMPTY_KEYS, bobSubKeys);
+//    
+//    sub = bobClient.subscribe ("require (From-Alice)");
+//    
+//    checkSecureSendReceive (aliceClient, sub);
+//    
+//    aliceClient.close ();
+//    bobClient.close ();
+  }
+
+  private static void checkSecureSendReceive (Elvin client,
+                                              final Subscription sub)
+    throws IOException, InterruptedException
+  {
+    sub.addNotificationListener (new NotificationListener ()
+    {
+      public void notificationReceived (NotificationEvent e)
+      {
+        assertTrue (e.secure);
+        
+        synchronized (sub)
+        {
+          sub.notifyAll ();
+        }
+      }
+    });
+    
+    Notification ntfn = new Notification ();
+    ntfn.put ("From-Alice", 1);
+    
+    synchronized (sub)
+    {
+      client.sendSecure (ntfn);
+      
+      wait (sub);
+    }
   }
   
   @Ignore
@@ -119,5 +254,16 @@ public class JUTestClient
     assertFalse (client.isConnected ());
     
     client.close ();
+  }
+  
+  private static void wait (Object sem)
+    throws InterruptedException
+  {
+    long waitStart = currentTimeMillis ();
+    
+    sem.wait (10000);
+    
+    if (currentTimeMillis () - waitStart >= 10000)
+      fail ("Timed out waiting for response");
   }
 }
