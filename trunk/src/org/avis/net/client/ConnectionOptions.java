1b2e1a727737d2a166701b06083cda71ecb94ca7
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-04-08 09:23:27 +0000

    Initial revision.

---
+++ trunk/src/org/avis/net/client/ConnectionOptions.java
@@ -0,0 +1,58 @@
+package org.avis.net.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.unmodifiableMap;
+
+import static org.avis.util.Util.valuesEqual;
+
+/**
+ * Connection options sent by the client to the server.
+ * 
+ * @author Matthew Phillips
+ */
+public class ConnectionOptions
+{
+  private HashMap<String, Object> values;
+
+  public ConnectionOptions ()
+  {
+    this.values = new HashMap<String, Object> ();
+  }
+  
+  public void set (String name, Object value)
+  {
+    values.put (name, value);
+  }
+  
+  public Object get (String name)
+  {
+    Object value = values.get (name);
+    
+    if (value == null)
+      throw new IllegalArgumentException ("No value for \"" + name + "\"");
+    else
+      return value;
+  }
+  
+  public Map<String, Object> differenceFrom (Map<String, Object> options)
+  {
+    HashMap<String, Object> diff = new HashMap<String, Object> ();
+    
+    for (Map.Entry<String, Object> entry : values.entrySet ())
+    {
+      Object actualValue = options.get (entry.getKey ());
+      
+      if (!valuesEqual (entry.getValue (), actualValue))
+        diff.put (entry.getKey (), actualValue);
+    }
+    
+    return diff;
+  }
+
+  public Map<String, Object> asMap ()
+  {
+    return unmodifiableMap (values);
+  }
+}
