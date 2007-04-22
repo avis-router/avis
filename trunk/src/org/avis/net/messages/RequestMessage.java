a25165f90ee90ff022cefb7aebc34d5fa71f451b
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-04-22 08:25:20 +0000

    Initial revision.

---
+++ trunk/src/org/avis/net/messages/RequestMessage.java
@@ -0,0 +1,29 @@
+package org.avis.net.messages;
+
+/**
+ * A XID-based request message that has a defined reply message.
+ * 
+ * @author Matthew Phillips
+ */
+public abstract class RequestMessage<R extends XidMessage> extends XidMessage
+{
+  public RequestMessage ()
+  {
+    super ();
+  }
+
+  public RequestMessage (XidMessage inReplyTo)
+  {
+    super (inReplyTo);
+  }
+
+  public RequestMessage (int xid)
+  {
+    super (xid);
+  }
+
+  /**
+   * The type of a successful reply.
+   */
+  public abstract Class<R> replyType ();
+}
