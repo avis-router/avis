a3dbf778b2d61c568d0a404960bea965079377e2
Matthew Phillips <mattphil@gmail.com>
2007-05-26 05:00:31 +0000

    Breaking into client, server and common packages.

--- server/src/test/org/avis/server/JUTestServer.java
+++ server/src/test/org/avis/server/JUTestServer.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-import org.avis.common.JUTestConnectionOptions;
 import org.avis.io.messages.ConfConn;
 import org.avis.io.messages.ConnRply;
 import org.avis.io.messages.ConnRqst;
