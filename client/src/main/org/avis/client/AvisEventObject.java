7b6e7f7a380507720f2a5d4692be76e418a9ba3a
Matthew Phillips <mattphil@gmail.com>
2007-06-03 02:47:18 +0000

    Initial revision.

---
+++ client/src/main/org/avis/client/AvisEventObject.java
@@ -0,0 +1,50 @@
+package org.avis.client;
+
+import java.util.EventObject;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Extends java.util.EventObject to add useful utilities such as
+ * general data association.
+ * 
+ * @author Matthew Phillips
+ */
+public abstract class AvisEventObject extends EventObject
+{
+  private Map<String, Object> data;
+
+  public AvisEventObject (Object source)
+  {
+    this (source, new HashMap<String, Object> ());
+  }
+  
+  public AvisEventObject (Object source, Map<String, Object> data)
+  {
+    super (source);
+    
+    this.data = data;
+  }
+
+  /**
+   * Set some generic data associated with the event.
+   * 
+   * @see #getData(String)
+   */
+  public void setData (String key, Object value)
+  {
+    data.put (key, value);
+  }
+  
+  /**
+   * Get some data previously associated with the event.
+   * 
+   * @return The data, or null if none set for the key.
+   * 
+   * @see #setData(String, Object)
+   */
+  public Object getData (String key)
+  {
+    return data.get (key);
+  }
+}
