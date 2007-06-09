715483adf91148a60a9681ecec7060f3fddd8c76
Matthew Phillips <mattphil@gmail.com>
2007-06-09 10:12:32 +0000

    Updated docs.

--- client/src/examples/org/avis/client/examples/WallBroadcaster.java
+++ client/src/examples/org/avis/client/examples/WallBroadcaster.java
@@ -10,26 +10,8 @@ import org.avis.client.Notification;
  * example, one or more "wall" broadcasters send typed characters from
  * the console to zero or more wall receivers.
  * 
- * Running The Example
- * 
- * You will need an Elvin router -- either install the Avis router (or
- * other compatible router) locally, or use the public Elvin router at
- * public.elvin.org.
- * 
- * To run a receiver from the console:
- * 
- * > ant example-wall-receiver
- * 
- * To run the broadcaster, open a new console and run:
- * 
- * > ant example-wall-broadcaster
- * 
- * Try running multiple broadcasters and receivers.
- * 
- * If you aren't using a local router (elvin://localhost), add
- * -Delvin=elvin://public.elvin.org to the commands, e.g.
- * 
- * > ant example-wall-broadcaster -Delvin=elvin://public.elvin.org
+ * See the documentation in "doc/examples.txt" for instructions on how to
+ * use the Avis examples.
  */
 public class WallBroadcaster
 {
@@ -46,11 +28,7 @@ public class WallBroadcaster
     elvin.closeOnExit ();
     
     System.out.println ("Type some lines of text to be broadcast over Elvin.");
-    
-    if (System.getProperty ("os.name").startsWith ("Windows"))
-      System.out.println ("Hit Ctrl+Z to exit.");
-    else
-      System.out.println ("Hit Ctrl+D to exit.");
+    System.out.println ("Hit Ctrl+C to exit.");
     
     // read from stdin one character at a time and send over Elvin
     InputStreamReader in = new InputStreamReader (System.in);
