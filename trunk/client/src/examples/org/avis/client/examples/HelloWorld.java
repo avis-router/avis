9ab4758091d48c46dc7db32f7136aa48f339120e
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-06-09 09:00:49 +0000

    Added docs.

--- trunk/client/src/examples/org/avis/client/examples/HelloWorld.java
+++ trunk/client/src/examples/org/avis/client/examples/HelloWorld.java
@@ -6,6 +6,30 @@ import org.avis.client.NotificationEvent;
 import org.avis.client.NotificationListener;
 import org.avis.client.Subscription;
 
+/*
+ * This example demonstrates two Elvin clients, one sending the
+ * traditional "Hello World" message as a greeting, the other one
+ * listening for all greetings and printing them to the console.
+ * 
+ * It also demonstrates an easy way to have one thread wait for
+ * another to receive a message. In this example, we use the
+ * subscription object to wait on the receipt of a greeting message.
+ * 
+ * Running The Example
+ * 
+ * You will need an Elvin router -- either install the Avis router (or
+ * other compatible router) locally, or use the public Elvin router at
+ * public.elvin.org. To run the example from the console:
+ * 
+ *   ant example-helloworld
+ * 
+ * or, if you aren't using a local router (elvin://localhost),
+ *   
+ *   ant example-helloworld -Delvin=elvin://public.elvin.org
+ */
+/**
+ * The HelloWorld example. See source code for more information.
+ */
 public class HelloWorld
 {
   public static void main (String [] args)
@@ -16,7 +40,7 @@ public class HelloWorld
     
     options.parseOrExit (args);
     
-    // create a client to listen for messages with a Greeting field
+    // create a client that listens for messages with a "Greeting" field
     Elvin listeningClient = new Elvin (options.elvinUri);
     
     final Subscription greetingSubscription =
@@ -26,10 +50,11 @@ public class HelloWorld
     {
       public void notificationReceived (NotificationEvent e)
       {
-        // let others into the greeting
-        System.out.println ("Greeting: " + e.notification.get ("Greeting"));
+        // show the greeting
+        System.out.println
+          ("Received greeting: " + e.notification.get ("Greeting"));
         
-        // notify the main thread that we got the message
+        // notify the waiting main thread that we got the message
         synchronized (greetingSubscription)
         {
           greetingSubscription.notify ();
@@ -37,10 +62,11 @@ public class HelloWorld
       }
     });
     
-    // create a client to send a greeting
+    // create a client that sends a greeting
     Elvin sendingClient = new Elvin (options.elvinUri);
     
     Notification greeting = new Notification ();
+    
     greeting.set ("Greeting", "Hello World!");    
     
     synchronized (greetingSubscription)
@@ -48,7 +74,7 @@ public class HelloWorld
       // send greeting...
       sendingClient.send (greeting);
       
-      // ... and wait for subscription to let us know it came through
+      // ... and wait for the listener to let us know it came through
       greetingSubscription.wait ();
     }
     
