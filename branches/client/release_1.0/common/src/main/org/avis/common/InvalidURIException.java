e5a48da47c6c5385577037af2ca3c29de80aee62
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-06-24 08:16:16 +0000

    Client release 1.0.

---
+++ branches/client/release_1.0/common/src/main/org/avis/common/InvalidURIException.java
@@ -0,0 +1,17 @@
+package org.avis.common;
+
+import java.net.URISyntaxException;
+
+/**
+ * Unchecked invalid URI exception. This is used in places that it
+ * would be irritating to use the checked Java {@link URISyntaxException}.
+ * 
+ * @author Matthew Phillips
+ */
+public class InvalidURIException extends RuntimeException
+{
+  public InvalidURIException (String uri, String message)
+  {
+    super ("Invalid URI: " + uri + ": " + message);
+  }
+}
