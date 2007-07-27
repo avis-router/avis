6e6c417b168532b51167d1fe57665197851ac640
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-07-27 06:58:44 +0000

    Lots 'o implementation work,

--- trunk/server/src/main/org/avis/federation/FederationConnector.java
+++ trunk/server/src/main/org/avis/federation/FederationConnector.java
@@ -9,6 +9,7 @@ import org.apache.mina.common.IoFutureListener;
 import org.apache.mina.common.IoHandler;
 import org.apache.mina.common.IoSession;
 import org.apache.mina.common.ThreadModel;
+import org.apache.mina.common.WriteFuture;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.transport.socket.nio.SocketConnector;
 import org.apache.mina.transport.socket.nio.SocketConnectorConfig;
@@ -16,12 +17,17 @@ import org.apache.mina.transport.socket.nio.SocketConnectorConfig;
 import org.avis.federation.messages.FedConnRply;
 import org.avis.federation.messages.FedConnRqst;
 import org.avis.federation.messages.FedModify;
+import org.avis.federation.messages.FedNotify;
+import org.avis.io.messages.Disconn;
 import org.avis.io.messages.Message;
 import org.avis.io.messages.Nack;
-import org.avis.logging.Log;
 import org.avis.router.Router;
 
+import static org.apache.mina.common.IoFutureListener.CLOSE;
+
 import static org.avis.federation.EwafURI.VERSION_MAJOR;
+import static org.avis.io.messages.Disconn.REASON_PROTOCOL_VIOLATION;
+import static org.avis.io.messages.Disconn.REASON_SHUTDOWN;
 import static org.avis.logging.Log.TRACE;
 import static org.avis.logging.Log.alarm;
 import static org.avis.logging.Log.shouldLog;
@@ -31,14 +37,24 @@ import static org.avis.logging.Log.warn;
 public class FederationConnector
   extends FederationLink implements IoHandler
 {
-  EwafURI uri;
-  IoSession connection;
-  SocketConnector connector;
-  SocketConnectorConfig connectorConfig;
-  FederationClass federationClass;
-  String remoteServerDomain;
-  boolean federationLinkConnected;
-  volatile boolean closing;
+  /**
+   * Internal disconnection reason code indicating we're shutting down
+   * on a Disconn request from the remote host.
+   */
+  private static final int REASON_SHUTDOWN_REQUESTED = -1;
+
+  private static final int STATE_HANDSHAKE = 0;
+  private static final int STATE_LINKED = 1;
+  private static final int STATE_CLOSING = 2;
+  
+  private EwafURI uri;
+  private IoSession connection;
+  private SocketConnector connector;
+  private SocketConnectorConfig connectorConfig;
+  private FederationClass federationClass;
+  private String remoteServerDomain;
+  
+  protected volatile int state;
   
   public FederationConnector (Router router, String serverDomain,
                               EwafURI uri, FederationClass federationClass)
@@ -48,6 +64,7 @@ public class FederationConnector
     this.uri = uri;
     this.federationClass = federationClass;
     this.connector = new SocketConnector (1, router.executor ());
+    this.state = STATE_HANDSHAKE;
 
     /* Change the worker timeout to make the I/O thread quit soon
      * when there's no connection to manage. */
@@ -76,7 +93,7 @@ public class FederationConnector
       {
         ConnectFuture future = (ConnectFuture)f;
         
-        if (!future.isConnected ())
+        if (state != STATE_CLOSING && !future.isConnected ())
           connect ();
       }
     });
@@ -84,10 +101,25 @@ public class FederationConnector
   
   public void close ()
   {
-    closing = true;
+    close (REASON_SHUTDOWN, "");
+  }
+  
+  private void close (int reason, String message)
+  {
+    if (state == STATE_CLOSING)
+      return;
+    
+    boolean wasLinked = state == STATE_LINKED;
+
+    state = STATE_CLOSING;
     
     if (isConnected ())
-      connection.close ().join (20000);
+    {
+      if (wasLinked)
+        send (new Disconn (reason, message)).addListener (CLOSE);
+      else
+        connection.close ();
+    }
   }
   
   public boolean isConnected ()
@@ -100,54 +132,80 @@ public class FederationConnector
     send (new FedConnRqst (VERSION_MAJOR, VERSION_MAJOR, serverDomain));
   }
   
+  /**
+   * Handle a message while in handshaking state.
+   */
   private void handleHandshakeMessage (Message message)
   {
     switch (message.typeId ())
     {
       case FedConnRply.ID:
         handleFedConnRply ((FedConnRply)message);
+        break;
       case Nack.ID:
         handleFedConnNack ((Nack)message);
+        break;
+      default:
+        warn ("Unexpected message during handshake from remote federator at " + 
+              uri + " (disconnecting): " + message.name (), this);
+        close (REASON_PROTOCOL_VIOLATION, "Unexpected " + message.name ());
     }
   }
   
+  /**
+   * Handle a message while in linked state.
+   */
   private void handleLinkMessage (Message message)
   {
     switch (message.typeId ())
     {
-      // todo
+      case FedModify.ID:
+        handleFedModify ((FedModify)message);
+        break;
+      case FedNotify.ID:
+        handleFedNotify ((FedNotify)message);
+        break;
+      case Disconn.ID:
+        close (REASON_SHUTDOWN_REQUESTED, "");
+      default:
+        warn ("Unexpected message from remote federator at " + uri + " (" +
+              "disconnecting): " + message.name (), this);
+        close (REASON_PROTOCOL_VIOLATION, "Unexpected " + message.name ());
     }
   }
   
   private void handleFedConnRply (FedConnRply reply)
   {
+    state = STATE_LINKED;
     remoteServerDomain = reply.serverDomain;
-    
-    startFederationLink ();
-  }
-  
-  private void startFederationLink ()
-  {
-    federationLinkConnected = true;
     
     send (new FedModify (federationClass.incomingFilter));
   }
-
+  
   private void handleFedConnNack (Nack nack)
   {
-    Log.warn ("Remote router at " + uri + 
-              " rejected federation connect request: " + 
-              nack.formattedMessage (), this);
+    warn ("Remote router at " + uri + 
+          " rejected federation connect request: " + 
+          nack.formattedMessage (), this);
     
     close ();
   }
 
-  private void send (Message message)
+  private void handleFedModify (FedModify message)
+  {
+    
+  }
+
+  private void handleFedNotify (FedNotify message)
+  {
+  }
+
+  private WriteFuture send (Message message)
   {
     if (shouldLog (TRACE))
       trace ("Federator " + serverDomain + " sent message: " +  message, this);
     
-    connection.write (message);
+    return connection.write (message);
   }
   
   // IoHandler  
@@ -155,9 +213,9 @@ public class FederationConnector
   public void messageReceived (IoSession session, Object message)
     throws Exception
   {
-    if (federationLinkConnected)
+    if (state == STATE_LINKED)
       handleLinkMessage ((Message)message);
-    else
+    else if (state == STATE_HANDSHAKE)
       handleHandshakeMessage ((Message)message);
   }
   
@@ -170,18 +228,15 @@ public class FederationConnector
   public void sessionClosed (IoSession session)
     throws Exception
   {
-    if (!closing)
+    if (state == STATE_LINKED)
     {
-      if (federationLinkConnected)
-      {
-        warn ("Router federator at " + uri + " " + 
-              "closed link with no warning", this);
-      } else
-      {
-        warn ("Router federator at " + uri + " " + 
-              "closed link during handshake, " +
-              "probably due to protocol violation", this);
-      }
+      warn ("Router federator at " + uri + " " + 
+            "closed link with no warning", this);
+    } else if (state == STATE_HANDSHAKE)
+    {
+      warn ("Router federator at " + uri + " " + 
+            "closed link during handshake, " +
+            "probably due to protocol violation", this);
     }
   }
   
