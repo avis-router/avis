1b40b36d9b4d771c7021b764494f8ea2c7ca4e6c
Matthew Phillips <mattphil@gmail.com>
2007-07-28 04:15:13 +0000

    Lots 'o implementation.

--- server/src/main/org/avis/federation/FederationListener.java
+++ server/src/main/org/avis/federation/FederationListener.java
@@ -1,47 +1,58 @@
 package org.avis.federation;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import java.io.Closeable;
 import java.io.IOException;
 
 import java.net.InetSocketAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 
 import org.apache.mina.common.DefaultIoFilterChainBuilder;
 import org.apache.mina.common.IdleStatus;
 import org.apache.mina.common.IoHandler;
 import org.apache.mina.common.IoSession;
 import org.apache.mina.common.ThreadModel;
+import org.apache.mina.common.WriteFuture;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
 
+import org.avis.federation.messages.FedConnRqst;
+import org.avis.io.messages.Disconn;
+import org.avis.io.messages.Message;
 import org.avis.router.Router;
 
-import static org.avis.io.Net.addressesFor;
+import static org.apache.mina.common.IoFutureListener.CLOSE;
+
+import static org.avis.federation.Federation.VERSION_MAJOR;
+import static org.avis.federation.Federation.VERSION_MINOR;
+import static org.avis.io.messages.Disconn.REASON_PROTOCOL_VIOLATION;
 import static org.avis.logging.Log.DIAGNOSTIC;
 import static org.avis.logging.Log.diagnostic;
 import static org.avis.logging.Log.internalError;
 import static org.avis.logging.Log.shouldLog;
+import static org.avis.logging.Log.warn;
 
-public class FederationListener implements IoHandler
+public class FederationListener implements IoHandler, Closeable
 {
-  /**
-   * todo
-   * 
-   * @param router
-   * @param federationId
-   * @param classMap
-   * @param listenUris
-   * @throws IOException 
-   * @throws UnknownHostException 
-   * @throws SocketException 
-   */
-  public FederationListener (Router router, String federationId,
-                             FederationClassMap classMap, 
-                             Set<EwafURI> listenUris)
+  private Router router;
+  private Set<FederationLink> links;
+  private Set<InetSocketAddress> addresses;
+  private String serverDomain;
+  private FederationClassMap federationClassMap;
+
+  public FederationListener (Router router,
+                             String serverDomain,
+                             FederationClassMap federationClassMap, 
+                             Set<InetSocketAddress> addresses)
     throws IOException
   {
+    this.router = router;
+    this.serverDomain = serverDomain;
+    this.federationClassMap = federationClassMap;
+    this.addresses = addresses;
+    this.links = new HashSet<FederationLink> ();
+    
     SocketAcceptorConfig acceptorConfig = new SocketAcceptorConfig ();
     
     acceptorConfig.setReuseAddress (true);
@@ -53,7 +64,7 @@ public class FederationListener implements IoHandler
     filterChainBuilder.addLast
       ("codec", new ProtocolCodecFilter (FederationFrameCodec.INSTANCE));
     
-    for (InetSocketAddress address : addressesFor (listenUris))
+    for (InetSocketAddress address : addresses)
     {
       if (shouldLog (DIAGNOSTIC))
         diagnostic ("Federator listening on address: " + address, this);
@@ -62,15 +73,116 @@ public class FederationListener implements IoHandler
     }
   }
 
-  // IoHandler
-  
-  public void sessionOpened (IoSession session)
-    throws Exception
+  public void close ()
   {
-    // todo
+    synchronized (this)
+    {
+      for (FederationLink link : links)
+        link.close ();
+      
+      links.clear ();
+    }
+    
+    for (InetSocketAddress address : addresses)
+      router.socketAcceptor ().unbind (address);
+    
+    addresses.clear ();
+  }
+  
+  private void handleHandshakeMessage (IoSession session,
+                                       Message message)
+  {
+    if (message instanceof FedConnRqst)
+    {
+      handleFedConnRqst (session, (FedConnRqst)message);
+    } else
+    {
+      warn ("Unexpected handshake message from connecting remote " +
+            "federator at " + remoteHostFor (session) + 
+            " (disconnecting): " + message.name (), this);
+      
+      session.close ();
+    }
   }
 
+  private void handleFedConnRqst (IoSession session, FedConnRqst message)
+  {
+    String remoteHost = remoteHostFor (session);
+    
+    if (message.versionMajor != VERSION_MAJOR || 
+        message.versionMinor > VERSION_MINOR)
+    {
+      String disconnMessage =
+        "Incompatible federation protocol version: " + 
+         message.versionMajor + "." + message.versionMinor +
+         " not compatible with " + VERSION_MAJOR + "." + VERSION_MINOR;
+      
+      warn ("Rejected federation request from " + 
+            remoteHost + ": " + disconnMessage, this);
+      
+      send (session,
+            new Disconn (REASON_PROTOCOL_VIOLATION, 
+                         disconnMessage)).addListener (CLOSE);
+    } else
+    {
+      createFederationLink
+        (session, message.serverDomain, remoteHost,
+         federationClassMap.classFor (remoteHost));
+    }
+  }
+  
+  private void createFederationLink (IoSession session,
+                                     String remoteServerDomain,
+                                     String remoteHost, 
+                                     FederationClass federationClass)
+  {
+    FederationLink link =
+      new FederationLink (session, router, federationClass, serverDomain, 
+                          remoteHost, remoteServerDomain);
+    
+    addLink (session, link);
+  }
+
+  private synchronized void addLink (IoSession session, FederationLink link)
+  {
+    session.setAttribute ("federationLink", link);
+    links.add (link);
+  }
+  
+  private static FederationLink linkFor (IoSession session)
+  {
+    return (FederationLink)session.getAttribute ("federationLink");
+  }
+
+  private static String remoteHostFor (IoSession session)
+  {
+    if (session.getRemoteAddress () instanceof InetSocketAddress)
+    {
+      InetSocketAddress address = 
+        (InetSocketAddress)session.getRemoteAddress ();
+      
+      return address.getHostName (); 
+    } else
+    {
+      throw new Error ("Can't get host name for address type " + 
+                       session.getRemoteAddress ().getClass ());
+    }
+  }
+
+  private WriteFuture send (IoSession session, Message message)
+  {
+    return Federation.send (session, serverDomain, message);
+  }
+  
+  // IoHandler
+  
   public void sessionCreated (IoSession session)
+    throws Exception
+  {
+    // zip
+  }
+
+  public void sessionOpened (IoSession session)
     throws Exception
   {
     // zip
@@ -79,7 +191,12 @@ public class FederationListener implements IoHandler
   public void messageReceived (IoSession session, Object message)
     throws Exception
   {
-    // todo
+    FederationLink link = linkFor (session);
+    
+    if (link == null)
+      handleHandshakeMessage (session, (Message)message);
+    else if (!link.isClosed ())
+      link.handleMessage ((Message)message);
   }
 
   public void sessionClosed (IoSession session)
@@ -97,12 +214,12 @@ public class FederationListener implements IoHandler
   public void messageSent (IoSession session, Object message)
     throws Exception
   {
-    // zio
+    // zip
   }
 
   public void sessionIdle (IoSession session, IdleStatus status)
     throws Exception
   {
-    // zip
+    // todo
   }
 }
