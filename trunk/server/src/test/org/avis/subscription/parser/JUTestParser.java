3b84c036fdec4abf97a442de03f70f8693386704
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-07-29 10:22:58 +0000

    Updated tests for new AST unparse format.

--- trunk/server/src/test/org/avis/subscription/parser/JUTestParser.java
+++ trunk/server/src/test/org/avis/subscription/parser/JUTestParser.java
@@ -45,10 +45,10 @@ public class JUTestParser
   public void strings ()
     throws Exception
   {
-    assertParsesTo ("'a\\n'", "(string 'an')");
-    assertParsesTo ("'a\\\\'", "(string 'a\\')");
-    assertParsesTo ("\"a\\\\!\"", "(string 'a\\!')");
-    assertParsesTo ("\"a\\\"\"", "(string 'a\"')");
+    assertParsesTo ("'a\\n'", "'an'");
+    assertParsesTo ("'a\\\\'", "'a\\'");
+    assertParsesTo ("\"a\\\\!\"", "'a\\!'");
+    assertParsesTo ("\"a\\\"\"", "'a\"'");
     
     assertParseError ("'a\\'");
   }
@@ -57,12 +57,12 @@ public class JUTestParser
   public void numbers ()
     throws Exception
   {
-    assertParsesTo ("0.0", "(real64 0.0)");
-    assertParsesTo ("3.4", "(real64 3.4)");
-    assertParsesTo ("1.2e10", "(real64 1.2E10)");
-    assertParsesTo ("1.2E10", "(real64 1.2E10)");
-    assertParsesTo ("1.2E+10", "(real64 1.2E10)");
-    assertParsesTo ("1.2E-10", "(real64 1.2E-10)");
+    assertParsesTo ("0.0", "0.0");
+    assertParsesTo ("3.4", "3.4");
+    assertParsesTo ("1.2e10", "1.2E10");
+    assertParsesTo ("1.2E10", "1.2E10");
+    assertParsesTo ("1.2E+10", "1.2E10");
+    assertParsesTo ("1.2E-10", "1.2E-10");
     
     assertParseError ("0.");
     assertParseError (".1");
@@ -80,9 +80,9 @@ public class JUTestParser
     throws Exception
   {
     assertParsesTo ("begins-with == 'hello'",
-                    "(== (field 'begins-with') (string 'hello'))");
+                    "(== (field 'begins-with') 'hello')");
     assertParsesTo ("int32 == 32",
-                    "(== (field 'int32') (int32 32))");
+                    "(== (field 'int32') 32)");
   }
   
   /**
@@ -93,13 +93,13 @@ public class JUTestParser
     throws Exception
   {
     assertParsesTo ("field1 > 2 && field2 == 'hello\\'there'",
-                    "(&& (> (field 'field1') (int32 2)) (== (field 'field2') " +
-                      "(string 'hello'there')))");
+                    "(&& (> (field 'field1') 2) (== (field 'field2') " +
+                      "'hello'there'))");
     
     assertParsesTo ("(field1 != 10L || field2 < 3.2) ^^ (field3 == \"hello\")",
-                    "(^^ (|| (! (== (field 'field1') (int64 10))) " +
-                      "(< (field 'field2') (real64 3.2))) " +
-                      "(== (field 'field3') (string 'hello')))");
+                    "(^^ (|| (! (== (field 'field1') 10L)) " +
+                      "(< (field 'field2') 3.2)) " +
+                      "(== (field 'field3') 'hello'))");
   }
   
   /**
@@ -111,21 +111,24 @@ public class JUTestParser
   {
     assertParsesTo ("size (name)", "(size 'name')");
     assertParsesTo ("require (name)", "(require 'name')");
-    assertParsesTo ("int32 (name)", "(int32 name)");
-    assertParsesTo ("begins-with (name, 'hello')", "(begins-with (field 'name') (string 'hello'))");
-    assertParsesTo ("ends-with (name, 'hello')", "(ends-with (field 'name') (string 'hello'))");
-    assertParsesTo ("contains (name, 'hello')", "(contains (field 'name') (string 'hello'))");
-    assertParsesTo ("regex (name, 'hello!+')", "(regex (field 'name') (string 'hello!+'))");
-    assertParsesTo ("wildcard (name, 'hel*lo?')", "(wildcard (field 'name') (string 'hel*lo?'))");
+    assertParsesTo ("int32 (name)", "(int32 'name')");
+    assertParsesTo ("int64 (name)", "(int64 'name')");
+    assertParsesTo ("real64 (name)", "(real64 'name')");
+    assertParsesTo ("string (name)", "(string 'name')");
+    assertParsesTo ("begins-with (name, 'hello')", "(begins-with (field 'name') 'hello')");
+    assertParsesTo ("ends-with (name, 'hello')", "(ends-with (field 'name') 'hello')");
+    assertParsesTo ("contains (name, 'hello')", "(contains (field 'name') 'hello')");
+    assertParsesTo ("regex (name, 'hello!+')", "(regex (field 'name') 'hello!+')");
+    assertParsesTo ("wildcard (name, 'hel*lo?')", "(wildcard (field 'name') 'hel*lo?')");
     assertParsesTo ("fold-case (name)", "(fold-case (field 'name'))");
     assertParsesTo ("decompose (name)", "(decompose (field 'name'))");
     assertParsesTo ("decompose-compat (name)", "(decompose-compat (field 'name'))");
-    assertParsesTo ("equals (name, 'hello')", "(== (field 'name') (string 'hello'))");
-    assertParsesTo ("equals ('hello', 'hello')", "(== (string 'hello') (string 'hello'))");
+    assertParsesTo ("equals (name, 'hello')", "(== (field 'name') 'hello')");
+    assertParsesTo ("equals ('hello', 'hello')", "(== 'hello' 'hello')");
     assertParsesTo ("equals (name, 'hello', 'there')",
-                    "(|| (== (field 'name') (string 'hello')) (== (field 'name') (string 'there')))");
+                    "(|| (== (field 'name') 'hello') (== (field 'name') 'there'))");
     assertParsesTo ("equals (name, 'hello', 42)",
-                    "(|| (== (field 'name') (string 'hello')) (== (field 'name') (int32 42)))");
+                    "(|| (== (field 'name') 'hello') (== (field 'name') 42))");
     
     assertParseError ("equals ('hello', 'there', 1)");
     assertParseError ("begins-with (name, 1)");
@@ -146,39 +149,39 @@ public class JUTestParser
   public void mathOps ()
     throws Exception
   {
-    assertParsesTo ("1 + 2", "(+ (int32 1) (int32 2))");
-    assertParsesTo ("1 + 2 + 3", "(+ (+ (int32 1) (int32 2)) (int32 3))");
-    assertParsesTo ("1 - 2", "(- (int32 1) (int32 2))");
-    assertParsesTo ("1 - 2 - 3", "(- (- (int32 1) (int32 2)) (int32 3))");
-    assertParsesTo ("1 - 2 + 3", "(- (int32 1) (+ (int32 2) (int32 3)))");
+    assertParsesTo ("1 + 2", "(+ 1 2)");
+    assertParsesTo ("1 + 2 + 3", "(+ (+ 1 2) 3)");
+    assertParsesTo ("1 - 2", "(- 1 2)");
+    assertParsesTo ("1 - 2 - 3", "(- (- 1 2) 3)");
+    assertParsesTo ("1 - 2 + 3", "(- 1 (+ 2 3))");
     
-    assertParsesTo ("1 * 2 / 3", "(* (int32 1) (/ (int32 2) (int32 3)))");
+    assertParsesTo ("1 * 2 / 3", "(* 1 (/ 2 3))");
 
-    assertParsesTo ("1 % 2", "(% (int32 1) (int32 2))");
+    assertParsesTo ("1 % 2", "(% 1 2)");
     
     // check "-" operator
-    assertParsesTo ("-2", "(- (int32 2))");
+    assertParsesTo ("-2", "(- 2)");
 
-    assertParsesTo ("~10", "(~ (int32 10))");
+    assertParsesTo ("~10", "(~ 10)");
 
-    assertParsesTo ("1 & 2", "(& (int32 1) (int32 2))");
-    assertParsesTo ("1 ^ 2", "(^ (int32 1) (int32 2))");
-    assertParsesTo ("1 | 2", "(| (int32 1) (int32 2))");
+    assertParsesTo ("1 & 2", "(& 1 2)");
+    assertParsesTo ("1 ^ 2", "(^ 1 2)");
+    assertParsesTo ("1 | 2", "(| 1 2)");
 
-    assertParsesTo ("1 << 2", "(<< (int32 1) (int32 2))");
-    assertParsesTo ("1 >> 2", "(>> (int32 1) (int32 2))");
-    assertParsesTo ("1 >>> 2", "(>>> (int32 1) (int32 2))");
+    assertParsesTo ("1 << 2", "(<< 1 2)");
+    assertParsesTo ("1 >> 2", "(>> 1 2)");
+    assertParsesTo ("1 >>> 2", "(>>> 1 2)");
 
     assertParsesTo ("(name1 & 0x0F) >> name2",
-                    "(>> (& (field 'name1') (int32 15)) (field 'name2'))");
+                    "(>> (& (field 'name1') 15) (field 'name2'))");
     
     assertParsesTo ("(name1 << 2L) | name2 & 0xF0",
-                    "(| (<< (field 'name1') (int64 2)) " +
-                      "(& (field 'name2') (int32 240)))");
+                    "(| (<< (field 'name1') 2L) " +
+                      "(& (field 'name2') 240))");
     
     // check math in equals ()
     assertParsesTo ("equals (name, 1 + 2)",
-                    "(== (field 'name') (+ (int32 1) (int32 2)))");
+                    "(== (field 'name') (+ 1 2))");
     
     // errors
     assertParseError ("1 + 'hello'");
