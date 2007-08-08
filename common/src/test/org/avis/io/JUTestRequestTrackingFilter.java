5f5960b8d17588980754791c8ff1669a23fadf72
Matthew Phillips <mattphil@gmail.com>
2007-08-08 07:53:22 +0000

    Moved network code to separate class.

--- common/src/test/org/avis/io/JUTestRequestTrackingFilter.java
+++ common/src/test/org/avis/io/JUTestRequestTrackingFilter.java
@@ -1,27 +1,12 @@
 package org.avis.io;
 
-import java.util.concurrent.ExecutorService;
-
-import java.net.InetSocketAddress;
-
-import org.apache.mina.common.ConnectFuture;
-import org.apache.mina.common.DefaultIoFilterChainBuilder;
 import org.apache.mina.common.IoHandler;
 import org.apache.mina.common.IoHandlerAdapter;
-import org.apache.mina.common.IoSession;
-import org.apache.mina.common.ThreadModel;
-import org.apache.mina.transport.socket.nio.SocketAcceptor;
-import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
-import org.apache.mina.transport.socket.nio.SocketConnector;
-import org.apache.mina.transport.socket.nio.SocketConnectorConfig;
-
-import org.avis.io.messages.ConnRqst;
-import org.avis.io.messages.Message;
-import org.avis.io.messages.RequestTimeoutMessage;
-
 import org.junit.Test;
 
-import static java.util.concurrent.Executors.newCachedThreadPool;
+import org.avis.io.messages.ConnRqst;
+import org.avis.io.messages.RequestTimeoutMessage;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
@@ -37,87 +22,53 @@ public class JUTestRequestTrackingFilter
   public void timeout () 
     throws Exception
   {
-    ExecutorService executor = newCachedThreadPool ();
-    InetSocketAddress remoteAddress = new InetSocketAddress ("0.0.0.0", 2917);
-    
-    // listener
-    SocketAcceptor acceptor = new SocketAcceptor (1, executor);
-    SocketAcceptorConfig acceptorConfig = new SocketAcceptorConfig ();
-    
-    acceptorConfig.setReuseAddress (true);
-    acceptorConfig.setThreadModel (ThreadModel.MANUAL);
-    
-    DefaultIoFilterChainBuilder filterChainBuilder =
-      acceptorConfig.getFilterChain ();
-
-    filterChainBuilder.addLast ("codec", ClientFrameCodec.FILTER);
-    
-    acceptor.bind (remoteAddress, new AcceptorListener (), acceptorConfig);
-    
-    // connector
-    SocketConnector connector = new SocketConnector (1, executor);
-    SocketConnectorConfig connectorConfig = new SocketConnectorConfig ();
-    
-    connector.setWorkerTimeout (0);
-    
-    connectorConfig.setThreadModel (ThreadModel.MANUAL);
-    connectorConfig.setConnectTimeout (20);
-    
-    connectorConfig.getFilterChain ().addLast   
-      ("codec", ClientFrameCodec.FILTER);
+    AcceptorConnectorSetup testSetup = new AcceptorConnectorSetup ();
     
     RequestTrackingFilter requestTrackingFilter = new RequestTrackingFilter (2);
     
-    connectorConfig.getFilterChain ().addLast 
+    testSetup.connectorConfig.getFilterChain ().addLast 
       ("tracker", requestTrackingFilter);
     
-    ConnectorListener listener = new ConnectorListener ();
+    TestingIoHandler connectListener = new TestingIoHandler ();
     
-    ConnectFuture future = 
-      connector.connect (remoteAddress, listener, connectorConfig);
-    
-    future.join ();
-    
-    IoSession session = future.getSession ();
+    testSetup.connect (new AcceptorListener (), connectListener);
     
     // send message, wait for timeout message
-    listener.message = null;
+    connectListener.message = null;
     
     ConnRqst fedConnRqst = new ConnRqst (1, 0);
     
-    session.write (fedConnRqst);
+    testSetup.session.write (fedConnRqst);
     
-    synchronized (listener)
+    synchronized (connectListener)
     {
-      listener.wait (3000);
+      connectListener.wait (3000);
     }
 
-    assertNotNull (listener.message);
-    assertEquals (RequestTimeoutMessage.ID, listener.message.typeId ());
-    assertSame (fedConnRqst, ((RequestTimeoutMessage)listener.message).request);
+    assertNotNull (connectListener.message);
+    assertEquals (RequestTimeoutMessage.ID, connectListener.message.typeId ());
+    assertSame (fedConnRqst, ((RequestTimeoutMessage)connectListener.message).request);
     
     // try again, with two in the pipe
-    listener.message = null;
+    connectListener.message = null;
     
     fedConnRqst = new ConnRqst (1, 0);
     
-    session.write (fedConnRqst);
+    testSetup.session.write (fedConnRqst);
     
     Thread.sleep (1000);
     
-    session.write (new ConnRqst (1, 0));
+    testSetup.session.write (new ConnRqst (1, 0));
     
-    synchronized (listener)
+    synchronized (connectListener)
     {
-      listener.wait (3000);
+      connectListener.wait (3000);
     }
 
-    assertNotNull (listener.message);
-    assertSame (fedConnRqst, ((RequestTimeoutMessage)listener.message).request);
+    assertNotNull (connectListener.message);
+    assertSame (fedConnRqst, ((RequestTimeoutMessage)connectListener.message).request);
     
-    session.close ();
-    acceptor.unbindAll ();
-    executor.shutdown ();
+    testSetup.close ();
     
     assertTrue (requestTrackingFilter.sharedResourcesDisposed ());
   }
@@ -125,20 +76,5 @@ public class JUTestRequestTrackingFilter
   static class AcceptorListener extends IoHandlerAdapter implements IoHandler
   {
     // zip
-  }
-  
-  static class ConnectorListener extends IoHandlerAdapter implements IoHandler
-  {
-    public Message message;
-
-    @Override
-    public synchronized void messageReceived (IoSession session, 
-                                              Object theMessage)
-      throws Exception
-    {
-      message = (Message)theMessage;
-      
-      notifyAll ();
-    }
   }
 }
