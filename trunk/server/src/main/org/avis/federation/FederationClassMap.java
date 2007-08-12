b805eb467a573674d6f7a460c6344497556a3b7a
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-08-12 10:06:16 +0000

    Initial implementation.

--- trunk/server/src/main/org/avis/federation/FederationClassMap.java
+++ trunk/server/src/main/org/avis/federation/FederationClassMap.java
@@ -1,8 +1,17 @@
 package org.avis.federation;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import java.net.InetAddress;
+
 public class FederationClassMap
 {
   private FederationClass defaultClass;
+  private Map<String, FederationClass> classes;
+  private Map<String, FederationClass> dnsDomains;
+  private Map<String, FederationClass> federatorDomains;
+  private Map<String, FederationClass> hosts;
 
   /**
    * Create a new instance.
@@ -12,19 +21,75 @@ public class FederationClassMap
   public FederationClassMap (FederationClass defaultClass)
   {
     this.defaultClass = defaultClass;
+    this.classes = new HashMap<String, FederationClass> ();
+    this.dnsDomains = new HashMap<String, FederationClass> ();
+    this.hosts = new HashMap<String, FederationClass> ();
+    this.federatorDomains = new HashMap<String, FederationClass> ();
   }
 
+  public FederationClass findOrCreate (String name)
+  {
+    FederationClass fedClass = classes.get (name);
+    
+    if (fedClass == null)
+      classes.put (name, fedClass = new FederationClass ());
+    
+    return fedClass;
+  }
+  
   /**
    * Get the federation class mapped to a given host.
    * 
-   * @param hostname The host's name.
+   * @param address The host's address.
+   * @param serverDomain The federation server domain.
    * 
    * @return The federation class, or defaultClass if no explicit
    *         mapping found.
    */
-  public FederationClass classFor (String hostname)
+  public FederationClass classFor (InetAddress address, String serverDomain)
   {
-    // todo
+    String canonicalHostName = address.getCanonicalHostName ();
+
+    // match host name/IP
+    FederationClass fedClass = hosts.get (canonicalHostName);
+    
+    if (fedClass != null)
+      return fedClass;
+    
+    fedClass = hosts.get (address.getHostAddress ());
+    
+    if (fedClass != null)
+      return fedClass;
+
+    // match domain
+    for (Map.Entry<String, FederationClass> entry : dnsDomains.entrySet ())
+    {
+      if (canonicalHostName.endsWith (entry.getKey ()))
+        return entry.getValue ();
+    }
+    
+    // match server domain
+    fedClass = federatorDomains.get (serverDomain);
+    
+    if (fedClass != null)
+      return fedClass;
+    
+    // fall back to default
     return defaultClass;
+  }
+  
+  public void mapDnsDomain (String domain, FederationClass fedClass)
+  {
+    dnsDomains.put (domain, fedClass);
+  }
+
+  public void mapHost (String hostname, FederationClass fedClass)
+  {
+    hosts.put (hostname, fedClass);
+  }
+  
+  public void mapServerDomain (String serverDomain, FederationClass fedClass)
+  {
+    federatorDomains.put (serverDomain, fedClass);
   }
 }
