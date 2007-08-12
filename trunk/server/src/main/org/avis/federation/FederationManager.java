fe8d7b9daf0f7f3213996c978db99e85fa42e324
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-08-12 05:40:46 +0000

    Initial revision.

---
+++ trunk/server/src/main/org/avis/federation/FederationManager.java
@@ -0,0 +1,12 @@
+package org.avis.federation;
+
+import org.avis.router.Router;
+import org.avis.util.Options;
+
+public class FederationManager
+{
+  public FederationManager (Router router, Options federationConfig)
+  {
+    // todo
+  }
+}
