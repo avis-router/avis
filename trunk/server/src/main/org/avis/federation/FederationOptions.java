d10aff666c1c694a9c79a8299a2fe35c048435b4
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-08-12 05:19:10 +0000

    Added core options.

--- trunk/server/src/main/org/avis/federation/FederationOptions.java
+++ trunk/server/src/main/org/avis/federation/FederationOptions.java
@@ -1,5 +1,9 @@
 package org.avis.federation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.avis.util.IllegalOptionException;
 import org.avis.util.OptionSet;
 import org.avis.util.Options;
 
@@ -9,9 +13,42 @@ public class FederationOptions extends Options
   
   static class FederationOptionSet extends OptionSet
   {
+    protected static final Pattern CUSTOM_OPTION_PATTERN = 
+      Pattern.compile ("([^:]+:?)([^:]+)?$");
+    
     public FederationOptionSet ()
     {
-      add ("Federation.Connection-Timeout", 1, 20, Integer.MAX_VALUE);
+      add ("Federation.Activated", false);
+      add ("Federation.Router-Name", "");
+      add ("Federation.Listen", "ewaf://0.0.0.0:2916");
+      add ("Federation.Connect-Timeout", 1, 20, Integer.MAX_VALUE);
+      add ("Federation.Subscribe:", "");
+      add ("Federation.Provide:", "");
+      add ("Federation.Connect:", "");
+    }
+    
+    /**
+     * Override validation to allow parameterised options like
+     * "Federation.Subscribe:Public".
+     */
+    @Override
+    protected void validateAndPut (Options options, String option,
+                                   Object value)
+      throws IllegalOptionException
+    {
+      Matcher matcher = CUSTOM_OPTION_PATTERN.matcher (option);
+      matcher.find ();
+      
+      String baseOption = matcher.group (1);
+      
+      value = convert (baseOption, value);
+      
+      String message = validate (baseOption, value);
+      
+      if (message != null)
+        throw new IllegalOptionException (option, message);
+      
+      set (options, option, value);
     }
   }
   
