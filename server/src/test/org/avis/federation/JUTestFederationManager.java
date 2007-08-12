ecd4e33ffb10e49941c130149e5e9ff7631b08e2
Matthew Phillips <mattphil@gmail.com>
2007-08-12 10:40:32 +0000

    Expanded tests. Passes!

--- server/src/test/org/avis/federation/JUTestFederationManager.java
+++ server/src/test/org/avis/federation/JUTestFederationManager.java
@@ -3,22 +3,48 @@ package org.avis.federation;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.net.InetAddress;
+
 import org.avis.io.messages.NotifyDeliver;
 import org.avis.router.Router;
 import org.avis.router.SimpleClient;
+import org.avis.util.LogFailTester;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import static java.lang.Thread.sleep;
+
+import static org.avis.io.Net.addressesFor;
+import static org.avis.util.Collections.set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class JUTestFederationManager
 {
   private static final int PORT1 = 29170;
   private static final int PORT2 = PORT1 + 10;
 
+  private LogFailTester logTester;
+
+  @Before
+  public void setup ()
+  {
+    // enableLogging (TRACE, true);
+    // enableLogging (DIAGNOSTIC, true);
+
+    logTester = new LogFailTester ();
+  }
+  
+  @After
+  public void tearDown ()
+  {
+    logTester.assertOkAndDispose ();
+  }
+  
   @Test
   public void basicListen () 
     throws Exception
@@ -37,6 +63,14 @@ public class JUTestFederationManager
     
     FederationManager manager = new FederationManager (router1, options);
     
+    FederationClass testClass = 
+      manager.classMap.classFor (InetAddress.getLocalHost (), "router2");
+    
+    assertFalse (testClass.isNull ());
+    
+    assertEquals (addressesFor (set (federationUri)),
+                  manager.listener.addresses);
+    
     Router router2 = new Router (PORT2);
     
     FederationClass fedClass =
@@ -50,26 +84,28 @@ public class JUTestFederationManager
     
     assertTrue (connector.isConnected ());
     
-//    SimpleClient client1 = new SimpleClient ("client1", "localhost", PORT1);
-//    SimpleClient client2 = new SimpleClient ("client2", "localhost", PORT2);
-//    
-//    client1.connect ();
-//    client2.connect ();
-//    
-//    client1.subscribe ("require (federated) && from == 'client2'");
-//    client2.subscribe ("require (federated) && from == 'client1'");
-//    
-//    client1.sendNotify 
-//      (map ("federated", "router1", "from", "client1"));
-//    
-//    NotifyDeliver notification = (NotifyDeliver)client2.receive ();
-//    
-//    assertEquals (0, notification.secureMatches.length);
-//    assertEquals (1, notification.insecureMatches.length);
-//    assertEquals ("client1", notification.attributes.get ("from"));
-//    
-//    client1.close ();
-//    client2.close ();
+    assertEquals (1, manager.listener.links.size ());
+    
+    SimpleClient client1 = new SimpleClient ("client1", "localhost", PORT1);
+    SimpleClient client2 = new SimpleClient ("client2", "localhost", PORT2);
+    
+    client1.connect ();
+    client2.connect ();
+    
+    client1.subscribe ("require (federated) && from == 'client2'");
+    client2.subscribe ("require (federated) && from == 'client1'");
+    
+    client1.sendNotify 
+      (map ("federated", "router1", "from", "client1"));
+    
+    NotifyDeliver notification = (NotifyDeliver)client2.receive ();
+    
+    assertEquals (0, notification.secureMatches.length);
+    assertEquals (1, notification.insecureMatches.length);
+    assertEquals ("client1", notification.attributes.get ("from"));
+    
+    client1.close ();
+    client2.close ();
     
     connector.close ();
     router2.close ();
