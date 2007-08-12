f061a50e70a2e211700efab4c9c94fb003dc4f68
Matthew Phillips <mattphil@gmail.com>
2007-08-12 05:41:07 +0000

    Updated.

--- server/src/test/org/avis/federation/JUTestFederationOptions.java
+++ server/src/test/org/avis/federation/JUTestFederationOptions.java
@@ -22,10 +22,14 @@ public class JUTestFederationOptions
     props.setProperty ("Federation.Provide:Internal", "TRUE");
     props.setProperty ("Federation.Connect:Internal", "ewaf://localhost");
     props.setProperty ("Federation.Connect:External", "ewaf://public.elvin.org");
+    props.setProperty ("Federation.Listen:External", ".elvin.org");
+    props.setProperty ("Federation.Listen", "ewaf://0.0.0.0");
     
     options.setAll (props);
     
     assertEquals (true, options.getBoolean ("Federation.Activated"));
+    assertEquals ("ewaf://0.0.0.0", options.get ("Federation.Listen"));
+    assertEquals (".elvin.org", options.get ("Federation.Listen:External"));
     assertEquals ("require ('Message')", 
                   options.getString ("Federation.Subscribe:External"));
   }
