0e437a0f1e8ecac4a61d2fad7eec4e268907b46b
Matthew Phillips <mattphil@gmail.com>
2007-08-18 02:58:46 +0000

    Added support for encoding CONST_FALSE as AST EMPTY.

--- server/src/test/org/avis/federation/JUTestAstXdrCoding.java
+++ server/src/test/org/avis/federation/JUTestAstXdrCoding.java
@@ -1,15 +1,19 @@
 package org.avis.federation;
 
 import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.filter.codec.ProtocolCodecException;
 
 import org.avis.subscription.ast.Node;
 
 import org.junit.Test;
 
 import static org.avis.subscription.ast.Nodes.unparse;
+import static org.avis.subscription.ast.nodes.Const.CONST_FALSE;
+import static org.avis.subscription.ast.nodes.Const.CONST_TRUE;
 import static org.avis.subscription.parser.SubscriptionParserBase.parse;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class JUTestAstXdrCoding
 {
@@ -26,13 +30,31 @@ public class JUTestAstXdrCoding
     roundtrip ("foobar == 'hello' || " +
     	       "greebo & 1 == 1 && begins-with (greebo, 'frob', 'wibble')");
     roundtrip ("size (foobar) > 10 && (foobar - 20 >= 100)");
+    
+    // check CONST_FALSE gets turned into EMPTY node
+    roundtrip (CONST_FALSE);
+
+    // can't do CONST_TRUE
+    try
+    {
+      roundtrip (CONST_TRUE);
+      
+      fail ();
+    } catch (ProtocolCodecException ex)
+    {
+      // ok
+    }
   }
   
   private static void roundtrip (String expr)
     throws Exception
   {
-    Node ast = parse (expr);
-    
+    roundtrip (parse (expr));
+  }
+  
+  private static void roundtrip (Node ast)
+    throws Exception
+  {
     ByteBuffer in = ByteBuffer.allocate (1024);
     
     AstXdrCoding.encodeAST (in, ast);
