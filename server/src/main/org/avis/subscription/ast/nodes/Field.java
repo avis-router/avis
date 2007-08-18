16f3d53ab0cadbb9502c446277c058580ffca010
Matthew Phillips <mattphil@gmail.com>
2007-08-18 06:08:11 +0000

    Override children () to return a Const.

--- server/src/main/org/avis/subscription/ast/nodes/Field.java
+++ server/src/main/org/avis/subscription/ast/nodes/Field.java
@@ -1,8 +1,12 @@
 package org.avis.subscription.ast.nodes;
 
+import java.util.Collection;
 import java.util.Map;
 
 import org.avis.subscription.ast.NameParentNode;
+import org.avis.subscription.ast.Node;
+
+import static java.util.Collections.singleton;
 
 public class Field extends NameParentNode
 {
@@ -32,5 +36,15 @@ public class Field extends NameParentNode
   public Object evaluate (Map<String, Object> attrs)
   {
     return attrs.get (name);
+  }
+  
+  /**
+   * Children of name-based nodes are by default Field's, but this
+   * would be recursive for a Field.
+   */
+  @Override
+  public Collection<Node> children ()
+  {
+    return singleton ((Node)new Const (name));
   }
 }
