d3704e7921e295c71c042378a0b2d5b9cfa89f7e
Matthew Phillips <mattphil@gmail.com>
2007-08-19 09:06:06 +0000

    Re-vamped to use paramterised options.

--- server/src/main/org/avis/federation/FederationOptions.java
+++ server/src/main/org/avis/federation/FederationOptions.java
@@ -1,54 +1,30 @@
 package org.avis.federation;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.avis.common.InvalidURIException;
+import org.avis.subscription.ast.Node;
+import org.avis.subscription.parser.ParseException;
 import org.avis.util.IllegalOptionException;
 import org.avis.util.OptionSet;
+import org.avis.util.OptionType;
 import org.avis.util.Options;
+import org.avis.util.Pair;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
+
+import static org.avis.federation.FederationClass.parse;
+import static org.avis.util.OptionSet.StringOption.ANY_STRING_OPTION;
 
 public class FederationOptions extends Options
 {
-  private static final Pattern CUSTOM_OPTION_PATTERN = 
-    Pattern.compile ("([^:]+:?)([^:]+)?$");
-  
   public static OptionSet OPTION_SET = new FederationOptionSet ();
 
-  static class FederationOptionSet extends OptionSet
-  {
-    public FederationOptionSet ()
-    {
-      add ("Federation.Activated", false);
-      add ("Federation.Router-Name", "");
-      add ("Federation.Listen", "ewaf://0.0.0.0:2916");
-      add ("Federation.Listen:", "");
-      add ("Federation.Subscribe:", "");
-      add ("Federation.Provide:", "");
-      add ("Federation.Connect:", "");
-      add ("Federation.Connect-Timeout", 1, 20, Integer.MAX_VALUE);
-    }
-    
-    /**
-     * Override validation to allow parameterised options like
-     * "Federation.Subscribe:Public".
-     */
-    @Override
-    protected void validateAndSet (Options options, String option,
-                                   Object value)
-      throws IllegalOptionException
-    {
-      String baseOption = splitOptionParam (option) [0];
-      
-      value = convert (baseOption, value);
-      
-      String message = validate (baseOption, value);
-      
-      if (message != null)
-        throw new IllegalOptionException (option, message);
-      
-      set (options, option, value);
-    }
-  }
+  private static final List<String> EMPTY_STRING_LIST = emptyList ();
   
   public FederationOptions ()
   {
@@ -56,14 +32,255 @@ public class FederationOptions extends Options
   }
 
   /**
+   * Get a value for a parameterised option. e.g.
+   * "Federation.Subscribe[Param]".
+   * 
+   * @param option The option, minus the parameters.
+   * 
+   * @return The value of the option, mapping parameters to values.
+   */
+  @SuppressWarnings("unchecked")
+  public Map<String, Object> getParamOption (String option)
+  {
+    OptionType type = optionSet.optionTypeFor (option);
+    
+    if (type instanceof ParamOption)
+      return (Map<String, Object>)get (option);
+    else
+      throw new IllegalOptionException (option, "Not a parameterised option");
+  }
+  
+  /**
    * Split a parameterised option into a (base option, param) pair.
    */
-  public static String [] splitOptionParam (String option)
+  protected static Pair<String,List<String>> splitOptionParam (String option)
   {
-    Matcher matcher = CUSTOM_OPTION_PATTERN.matcher (option);
+    int index = option.indexOf ('[');
     
-    matcher.find ();
+    if (index == -1)
+    {
+      if (option.indexOf (']') == -1)
+        return new Pair<String, List<String>> (option, EMPTY_STRING_LIST);
+      else
+        throw new IllegalOptionException (option, "Orphan ]");
+    }
     
-    return new String [] {matcher.group (1), matcher.group (2)};
+    String base = option.substring (0, index);
+    List<String> params = new ArrayList<String> ();
+
+    while (index < option.length ())
+    {
+      int end = option.indexOf (']', index);
+      
+      if (end == -1)
+        throw new IllegalOptionException (option, "Missing ]");
+      
+      params.add (option.substring (index + 1, end));
+      
+      index = end + 1;
+      
+      if (index < option.length () && option.charAt (index) != '[')
+          throw new IllegalOptionException (option, "Junk in parameter list");
+    }
+    
+    return new Pair<String, List<String>> (base, params);
+  }
+  
+  static class FederationOptionSet extends OptionSet
+  {
+    public FederationOptionSet ()
+    {
+      ParamOption fedClassOption = new ParamOption (new SubExpOption ());
+      EwafUriOption ewafUriOption = new EwafUriOption ();
+      
+      add ("Federation.Activated", false);
+      add ("Federation.Router-Name", "");
+      add ("Federation.Listen", ewafUriOption, 
+           new EwafURI ("ewaf://0.0.0.0:2916"));
+      add ("Federation.Subscribe", fedClassOption, emptyMap ());
+      add ("Federation.Provide", fedClassOption, emptyMap ());
+      add ("Federation.Apply-Class", 
+           new ParamOption (ANY_STRING_OPTION), emptyMap ());
+      add ("Federation.Connect", 
+           new ParamOption (ewafUriOption), emptyMap ());
+      add ("Federation.Connect-Timeout", 1, 20, Integer.MAX_VALUE);
+    }
+    
+    /**
+     * Override validation to allow parameterised options like
+     * "Federation.Subscribe[Public]".
+     */
+    @Override
+    protected void validateAndSet (Options options, String option,
+                                   Object value)
+      throws IllegalOptionException
+    {
+      Pair<String, List<String>> optionItems = splitOptionParam (option);
+      
+      OptionType type = optionTypeFor (optionItems.item1);
+      
+      if (type instanceof ParamOption)
+      {
+        // allow param option to create/update param values
+        value = 
+          ((ParamOption)type).updateValue (options, option, 
+                                           optionItems.item1,
+                                           optionItems.item2, value);
+      } else
+      {
+        if (!optionItems.item2.isEmpty ())
+        {
+          throw new IllegalOptionException
+            (option, "Cannot specify parameters for option");
+        }
+      }
+
+      super.validateAndSet (options, optionItems.item1, value);
+    }
+  }
+  
+  /**
+   * An subscription expression option.
+   */
+  static class SubExpOption extends OptionType
+  {
+    @Override
+    public Object convert (String option, Object value)
+      throws IllegalOptionException
+    {
+      try
+      {
+        if (value instanceof Node)
+          return value;
+        else
+          return parse (value.toString ());
+      } catch (ParseException ex)
+      {
+        throw new IllegalOptionException 
+          (option, "Invalid subscription: " + ex.getMessage ());
+      }
+    }
+
+    @Override
+    public String validate (String option, Object value)
+    {
+      return validateType (value, Node.class);
+    }
+  }
+
+  /**
+   * An EWAF URI option.
+   */
+  static class EwafUriOption extends OptionType
+  {
+    @Override
+    public Object convert (String option, Object value)
+      throws IllegalOptionException
+    {
+      try
+      {
+        if (!(value instanceof EwafURI))
+          value = new EwafURI (value.toString ());
+        
+        return value;
+      } catch (InvalidURIException ex)
+      {
+        throw new IllegalOptionException 
+          (option, "Invalid URI: " + ex.getMessage ());
+      }
+    }
+
+    @Override
+    public String validate (String option, Object value)
+    {
+      return validateType (value, EwafURI.class);
+    }
+  }
+  
+  /**
+   * A parameterised option. These options have a Map as a value,
+   * mapping parameters to their values (which may themselves be maps
+   * for multi-dimensional options).
+   */
+  static class ParamOption extends OptionType
+  {
+    private OptionType subOption;
+    private int paramCount;
+
+    public ParamOption (OptionType option)
+    {
+      this (option, 1);
+    }
+
+    public ParamOption (OptionType subOption, int paramCount)
+    {
+      this.subOption = subOption;
+      this.paramCount = paramCount;
+    }
+
+    @Override
+    public Object convert (String option, Object value)
+      throws IllegalOptionException
+    {
+      return value;
+    }
+
+    @Override
+    public String validate (String option, Object value)
+    {
+      return validateType (value, Map.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Object updateValue (Options options, 
+                               String option, 
+                               String baseOption,
+                               List<String> params, Object value)
+    {
+      if (params.size () != paramCount)
+        throw new IllegalOptionException (option, 
+                                          "Parameters required " + paramCount);
+      
+      value = subOption.convert (option, value);
+
+      String valid = subOption.validate (option, value);
+      
+      if (valid != null)
+        throw new IllegalOptionException (option, valid);
+      
+      // create/lookup base Map value
+      Map<String, Object> baseValue = 
+        (Map<String, Object>)options.get (baseOption);
+      
+      if (baseValue.isEmpty ())
+        baseValue = new HashMap<String, Object> ();
+      
+      // create/lookup any sub-map's
+      Map<String, Object> map = baseValue;
+      
+      for (int i = paramCount; i > 1; i--)
+        map = createEntry (map, params.get (i));
+      
+      // store value
+      map.put (params.get (params.size () - 1), value);
+      
+      return baseValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<String, Object> createEntry (Map<String, Object> map,
+                                                    String key)
+    {
+      Map<String, Object> entry = (Map<String, Object>)map.get (key);
+      
+      if (entry == null)
+      {
+        entry = new HashMap<String, Object> ();
+        
+        map.put (key, entry);
+      }
+      
+      return entry;
+    }
   }
 }
