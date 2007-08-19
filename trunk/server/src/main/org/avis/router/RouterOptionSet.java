d437344284f44da55513f49a621fb2677e19187d
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-08-19 10:24:29 +0000

    Tweakage.

--- trunk/server/src/main/org/avis/router/RouterOptionSet.java
+++ trunk/server/src/main/org/avis/router/RouterOptionSet.java
@@ -3,6 +3,7 @@ package org.avis.router;
 import org.avis.config.OptionSet;
 
 import static org.avis.common.Common.DEFAULT_PORT;
+import static org.avis.router.ConnectionOptionSet.CONNECTION_OPTION_SET;
 
 public class RouterOptionSet extends OptionSet
 {
@@ -11,6 +12,6 @@ public class RouterOptionSet extends OptionSet
     add ("Port", 1, DEFAULT_PORT, 65535);
     add ("Listen", "elvin://0.0.0.0");
     
-    inheritFrom (ConnectionOptionSet.CONNECTION_OPTION_SET);
+    inheritFrom (CONNECTION_OPTION_SET);
   }
 }
