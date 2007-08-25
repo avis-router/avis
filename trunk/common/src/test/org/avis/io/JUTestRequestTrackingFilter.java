d9aa80d0c2eeb74598f2b73e4d03e5aa090bd4c1
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-08-25 09:51:55 +0000

    Added liveness check.

--- trunk/common/src/test/org/avis/io/JUTestRequestTrackingFilter.java
+++ trunk/common/src/test/org/avis/io/JUTestRequestTrackingFilter.java
@@ -4,8 +4,13 @@ import org.apache.mina.common.IoHandler;
 import org.apache.mina.common.IoHandlerAdapter;
 import org.junit.Test;
 
+import static java.lang.Thread.sleep;
+
 import org.avis.io.messages.ConnRqst;
+import org.avis.io.messages.LivenessTimeoutMessage;
 import org.avis.io.messages.RequestTimeoutMessage;
+import org.avis.io.messages.TestConn;
+import org.avis.logging.Log;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -19,19 +24,20 @@ public class JUTestRequestTrackingFilter
    * timeout message.
    */
   @Test
-  public void timeout () 
+  public void requestTimeout () 
     throws Exception
   {
     AcceptorConnectorSetup testSetup = new AcceptorConnectorSetup ();
     
-    RequestTrackingFilter requestTrackingFilter = new RequestTrackingFilter (2);
+    RequestTrackingFilter requestTrackingFilter = 
+      new RequestTrackingFilter (2, Integer.MAX_VALUE);
     
     testSetup.connectorConfig.getFilterChain ().addLast 
       ("tracker", requestTrackingFilter);
     
     TestingIoHandler connectListener = new TestingIoHandler ();
     
-    testSetup.connect (new AcceptorListener (), connectListener);
+    testSetup.connect (new NullAcceptorListener (), connectListener);
     
     // send message, wait for timeout message
     connectListener.message = null;
@@ -40,10 +46,7 @@ public class JUTestRequestTrackingFilter
     
     testSetup.session.write (fedConnRqst);
     
-    synchronized (connectListener)
-    {
-      connectListener.wait (3000);
-    }
+    connectListener.waitForMessage ();
 
     assertNotNull (connectListener.message);
     assertEquals (RequestTimeoutMessage.ID, connectListener.message.typeId ());
@@ -60,10 +63,7 @@ public class JUTestRequestTrackingFilter
     
     testSetup.session.write (new ConnRqst (1, 0));
     
-    synchronized (connectListener)
-    {
-      connectListener.wait (3000);
-    }
+    connectListener.waitForMessage ();
 
     assertNotNull (connectListener.message);
     assertSame (fedConnRqst, ((RequestTimeoutMessage)connectListener.message).request);
@@ -73,7 +73,48 @@ public class JUTestRequestTrackingFilter
     assertTrue (requestTrackingFilter.sharedResourcesDisposed ());
   }
   
-  static class AcceptorListener extends IoHandlerAdapter implements IoHandler
+  /**
+   * Send a message check that RequestTracker generates a
+   * TestConn message and sends LivenessTimeout when no reponse.
+   */
+  @Test
+  public void livenessTimeout () 
+    throws Exception
+  {
+    Log.enableLogging (Log.TRACE, true);
+    Log.enableLogging (Log.DIAGNOSTIC, true);
+    
+    AcceptorConnectorSetup testSetup = new AcceptorConnectorSetup ();
+    
+    RequestTrackingFilter requestTrackingFilter = 
+      new RequestTrackingFilter (1, 1);
+    
+    testSetup.connectorConfig.getFilterChain ().addLast 
+      ("tracker", requestTrackingFilter);
+    
+    TestingIoHandler acceptorListener = new TestingIoHandler ();
+    TestingIoHandler connectListener = new TestingIoHandler ();
+    
+    testSetup.connect (acceptorListener, connectListener);
+    
+    // wait for TestConn message
+    acceptorListener.waitForMessage ();
+
+    assertEquals (TestConn.ID, acceptorListener.message.typeId ());
+    
+    sleep (1000);
+    
+    // wait for LivenessTimeout
+    connectListener.waitForMessage ();
+    assertEquals (LivenessTimeoutMessage.ID, connectListener.message.typeId ());
+    
+    testSetup.close ();
+    
+    assertTrue (requestTrackingFilter.sharedResourcesDisposed ());
+  }
+  
+  static class NullAcceptorListener 
+    extends IoHandlerAdapter implements IoHandler
   {
     // zip
   }
