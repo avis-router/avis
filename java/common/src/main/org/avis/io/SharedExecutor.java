8b302c89e679f28000418882ce4b1a99e8cc361e
Matthew Phillips <mattphil@gmail.com>
2007-10-29 11:05:18 +0000

    Fixed bug in aquire ()'s sync block.

--- java/common/src/main/org/avis/io/SharedExecutor.java
+++ java/common/src/main/org/avis/io/SharedExecutor.java
@@ -24,7 +24,7 @@ public final class SharedExecutor
    */
   public static ScheduledExecutorService acquire ()
   {
-    synchronized (LivenessFilter.class)
+    synchronized (SharedExecutor.class)
     {
       if (shareCount++ == 0)
         sharedExecutor = newScheduledThreadPool (1);
