4270a4096f3a5ce8a9c2765405358273f8e58628
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2007-12-01 04:30:19 +0000

    Initial TLS support.

--- trunk/java/server/src/main/org/avis/router/RouterOptionSet.java
+++ trunk/java/server/src/main/org/avis/router/RouterOptionSet.java
@@ -1,8 +1,10 @@
 package org.avis.router;
 
 import org.avis.config.OptionSet;
+import org.avis.config.OptionTypeURI;
 
 import static org.avis.common.Common.DEFAULT_PORT;
+import static org.avis.io.Net.uri;
 import static org.avis.router.ConnectionOptionSet.CONNECTION_OPTION_SET;
 
 public class RouterOptionSet extends OptionSet
@@ -11,6 +13,8 @@ public class RouterOptionSet extends OptionSet
   {
     add ("Port", 1, DEFAULT_PORT, 65535);
     add ("Listen", "elvin://0.0.0.0");
+    add ("TLS.Router-Keystore", new OptionTypeURI (), uri ("router.ks"));
+    add ("TLS.Router-Keystore.Passphrase", "");
     
     inheritFrom (CONNECTION_OPTION_SET);
   }
