7b2aa106b9e148bff9883ec422e2ef1f3e437bed
Matthew Phillips <mattphil@gmail.com>
2007-12-14 00:58:46 +0000

    Federation.Connect now allows more than one URI.

--- java/server/src/test/org/avis/federation/JUTestFederationOptions.java
+++ java/server/src/test/org/avis/federation/JUTestFederationOptions.java
@@ -5,20 +5,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.junit.Test;
+
 import org.avis.config.OptionTypeParam;
 import org.avis.config.Options;
 import org.avis.subscription.ast.Node;
 import org.avis.subscription.parser.ParseException;
-import org.avis.util.IllegalOptionException;
+import org.avis.util.IllegalConfigOptionException;
 import org.avis.util.Pair;
-
-import org.junit.Test;
 
 import static org.avis.federation.FederationClass.parse;
 import static org.avis.subscription.ast.Nodes.unparse;
 import static org.avis.util.Collections.list;
 import static org.avis.util.Collections.set;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -38,7 +37,8 @@ public class JUTestFederationOptions
     props.setProperty ("Federation.Subscribe[Internal]", "TRUE");
     props.setProperty ("Federation.Subscribe[External]", "require (Message)");
     props.setProperty ("Federation.Provide[Internal]", "TRUE");
-    props.setProperty ("Federation.Connect[Internal]", "ewaf://localhost");
+    props.setProperty ("Federation.Connect[Internal]", 
+                       "ewaf://localhost ewaf://public");
     props.setProperty ("Federation.Connect[External]", 
                        "ewaf://public.elvin.org");
     props.setProperty ("Federation.Apply-Class[External]", 
@@ -65,7 +65,7 @@ public class JUTestFederationOptions
                        new EwafURI ("ewaf://hello:7778")), 
                   options.get ("Federation.Listen"));
     
-    Map<String, Object> subscribe = 
+    Map<String, ?> subscribe = 
       options.getParamOption ("Federation.Subscribe");
     
     assertEquals (astExpr ("require (Message)"), 
@@ -74,24 +74,25 @@ public class JUTestFederationOptions
     assertEquals (astExpr ("TRUE"), 
                   unparse ((Node)subscribe.get ("Internal")));
     
-    Map<String, Object> connect = 
+    Map<String, ?> connect = 
       options.getParamOption ("Federation.Connect");
     
-    assertEquals (new EwafURI ("ewaf://localhost"), 
+    assertEquals (set (new EwafURI ("ewaf://localhost"), 
+                       new EwafURI ("ewaf://public")), 
                   connect.get ("Internal"));
     
-    assertEquals (new EwafURI ("ewaf://public.elvin.org"), 
+    assertEquals (set (new EwafURI ("ewaf://public.elvin.org")), 
                   connect.get ("External"));
     
-    Map<String, Object> applyClass = 
+    Map<String, ?> applyClass = 
       options.getParamOption ("Federation.Apply-Class");
     
     assertEquals (set ("@.elvin.org", "domain"),  applyClass.get ("External"));
     
-    assertEquals (new EwafURI ("ewaf://public.elvin.org"), 
+    assertEquals (set (new EwafURI ("ewaf://public.elvin.org")), 
                   connect.get ("External"));
     
-    Map<String, Object> addIncomingAttribute = 
+    Map<String, ?> addIncomingAttribute = 
       options.getParamOption ("Federation.Add-Incoming-Attribute");
     
     Map<String, Object> incomingAttrs =
@@ -104,7 +105,7 @@ public class JUTestFederationOptions
     assertTrue (Arrays.equals (new byte [] {(byte)0xde, (byte)0xad}, 
                                (byte [])incomingAttrs.get ("Opaque")));
     
-    Map<String, Object> addOutgoingAttribute = 
+    Map<String, ?> addOutgoingAttribute = 
       options.getParamOption ("Federation.Add-Outgoing-Attribute");
     
     Map<String, Object> outgoingAttrs =
@@ -156,7 +157,7 @@ public class JUTestFederationOptions
       OptionTypeParam.splitOptionParam (expr);
       
       fail ();
-    } catch (IllegalOptionException ex)
+    } catch (IllegalConfigOptionException ex)
     {
       // ok
     }
