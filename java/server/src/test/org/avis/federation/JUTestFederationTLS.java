2c28a632820ed43f5527fd7740d17a39260641de
Matthew Phillips <mattphil@gmail.com>
2007-12-19 01:25:37 +0000

    Added full connect/accept test.

--- java/server/src/test/org/avis/federation/JUTestFederationTLS.java
+++ java/server/src/test/org/avis/federation/JUTestFederationTLS.java
@@ -2,9 +2,6 @@ package org.avis.federation;
 
 import java.net.InetSocketAddress;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-
 import org.apache.mina.common.ConnectFuture;
 import org.apache.mina.common.IoHandler;
 import org.apache.mina.common.IoSession;
@@ -15,7 +12,6 @@ import org.apache.mina.transport.socket.nio.SocketConnectorConfig;
 
 import org.avis.federation.io.FederationFrameCodec;
 import org.avis.federation.io.messages.FedConnRqst;
-import org.avis.io.AvisX509TrustManager;
 import org.avis.io.TestingIoHandler;
 import org.avis.router.Router;
 import org.avis.router.RouterOptionSet;
@@ -26,7 +22,10 @@ import org.junit.Test;
 import static org.avis.federation.Federation.VERSION_MAJOR;
 import static org.avis.federation.Federation.VERSION_MINOR;
 import static org.avis.federation.JUTestFederation.PORT1;
+import static org.avis.federation.JUTestFederation.PORT2;
+import static org.avis.federation.TestUtils.waitForConnect;
 import static org.avis.federation.TestUtils.waitForSingleLink;
+import static org.avis.io.TLS.defaultSSLContext;
 import static org.avis.util.Collections.set;
 
 public class JUTestFederationTLS
@@ -73,6 +72,58 @@ public class JUTestFederationTLS
   }
   
   /**
+   * Full connect/accept test over TLS.
+   */
+  @Test
+  public void full () 
+    throws Exception
+  {
+    FederationClass fedClass = StandardFederatorSetup.defaultClass ();
+    EwafURI ewafURI = new EwafURI ("ewaf:/secure/127.0.0.1:" + (PORT1 + 1));
+   
+    // router1 (listener)
+    FederationClasses classes = new FederationClasses ();
+    
+    classes.mapServerDomain ("server2", fedClass);
+    
+    RouterOptionSet routerOptionSet = new RouterOptionSet ();
+    
+    routerOptionSet.inheritFrom (FederationOptionSet.OPTION_SET);
+    
+    RouterOptions options1 = new RouterOptions (routerOptionSet);
+    
+    options1.set ("Listen", "elvin://127.0.0.1:" + PORT1);
+    options1.set ("TLS.Keystore", getClass ().getResource ("router.ks"));
+    options1.set ("TLS.Keystore-Passphrase", "testing");
+    
+    Router router1 = new Router (options1);
+
+    Acceptor acceptor = 
+      new Acceptor (router1, "server1", classes, set (ewafURI), options1);
+    
+    // router 2 (connector)
+    RouterOptions options2 = new RouterOptions (routerOptionSet);
+    
+    options2.set ("Listen", "elvin://127.0.0.1:" + PORT2);
+    options2.set ("TLS.Keystore", getClass ().getResource ("router.ks"));
+    options2.set ("TLS.Keystore-Passphrase", "testing");
+    
+    Router router2 = new Router (options2);
+
+    Connector connector = 
+      new Connector (router2, "server2", ewafURI, fedClass, options2);
+    
+    waitForConnect (connector);
+    waitForSingleLink (acceptor);
+    
+    connector.close ();
+    acceptor.close ();
+    
+    router1.close ();
+    router2.close ();
+  }
+  
+  /**
    * Create a connection to federation listener.
    */
   private static IoSession connectFederationTLS (Router router,
@@ -88,14 +139,7 @@ public class JUTestFederationTLS
     connectorConfig.setThreadModel (ThreadModel.MANUAL);
     connectorConfig.setConnectTimeout (20);
     
-    AvisX509TrustManager trustManager = 
-      new AvisX509TrustManager (null, false, false);
-    
-    SSLContext sslContext = SSLContext.getInstance ("TLS");
-
-    sslContext.init (null, new TrustManager [] {trustManager}, null);
-
-    SSLFilter filter = new SSLFilter (sslContext);
+    SSLFilter filter = new SSLFilter (defaultSSLContext ());
     
     filter.setUseClientMode (true);
     
