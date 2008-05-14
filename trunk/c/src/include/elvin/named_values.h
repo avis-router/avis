87fec35ba7d096f6adbc904e7511485a7230ca97
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2008-05-14 05:49:44 +0000

    Merged MINA 2.0 conversion branch to trunk (merge -r 2183:HEAD https://avis.svn.sourceforge.net/svnroot/avis/branches/sandbox/mina_2.0_conversion).

---
+++ trunk/c/src/include/elvin/named_values.h
@@ -0,0 +1,20 @@
+/*
+ *  named_values.h
+ *  Elvin Client
+ *
+ *  Created by Matthew Phillips on 13/05/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef ELVIN_NAMED_VALUES_H
+#define ELVIN_NAMED_VALUES_H
+
+// TODO
+#define EMPTY_NAMED_VALUES NULL
+
+typedef struct 
+{
+} Named_Values;
+
+#endif // ELVIN_NAMED_VALUES_H
