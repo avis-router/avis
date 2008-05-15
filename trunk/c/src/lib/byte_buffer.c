2878302ddbeb50393fc690f84332a854b97e0df0
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2008-05-15 04:52:42 +0000

    Initial revision.

---
+++ trunk/c/src/lib/byte_buffer.c
@@ -0,0 +1,23 @@
+#include "byte_buffer.h"
+
+#define INIT_LENGTH 1024
+
+Byte_Buffer *byte_buffer_alloc ()
+{
+  Byte_Buffer *buffer = malloc (sizeof (Byte_Buffer));
+  
+  buffer->data_length = INIT_LENGTH;
+  buffer->data = malloc (INIT_LENGTH);
+  
+  return buffer;
+}
+
+void byte_buffer_dealloc (Byte_Buffer *buffer)
+{
+  free (buffer->data);
+}
+
+size_t byte_buffer_len (Byte_Buffer *buffer)
+{
+  return buffer->data_length;
+}
---
+++ trunk/c/src/lib/byte_buffer.h
@@ -0,0 +1,17 @@
+#ifndef BYTE_BUFFER_H_
+#define BYTE_BUFFER_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct
+{
+  size_t data_length;
+  uint8_t *data;
+} Byte_Buffer;
+
+Byte_Buffer *byte_buffer_alloc ();
+void byte_buffer_dealloc (Byte_Buffer *buffer);
+size_t byte_buffer_len (Byte_Buffer *buffer);
+
+#endif /*BYTE_BUFFER_H_*/
