b6133864338dd48640554811ea34a9b81af52e8b
Matthew Phillips <mattphil@gmail.com>
2008-05-15 04:52:42 +0000

    Initial revision.

---
+++ c/src/test/check_ext.c
@@ -0,0 +1,6 @@
+#include "check_ext.h"
+
+void fail_on_error (Elvin_Error *error)
+{
+  // todo
+}
---
+++ c/src/test/check_ext.h
@@ -0,0 +1,8 @@
+#ifndef CHECK_EXT_H_
+#define CHECK_EXT_H_
+
+#include <elvin/errors.h>
+
+void fail_on_error (Elvin_Error *error);
+
+#endif /*CHECK_EXT_H_*/
