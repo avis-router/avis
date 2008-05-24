c3f9a76bfb09cb4a93646dff0491ecbdc131beb8
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2008-05-24 05:29:42 +0000

    Initial revision.

---
+++ trunk/c/src/include/elvin/values.h
@@ -0,0 +1,53 @@
+#ifndef VALUES_H_
+#define VALUES_H_
+
+#include <stdint.h>
+
+typedef double real64_t;
+
+/**
+ * The type tag for polymorphic values. These are the same as the type codes 
+ * from the client protocol spec.
+ */
+typedef enum 
+{
+  TYPE_INT32 = 1, TYPE_INT64 = 2, TYPE_REAL64 = 3, 
+  TYPE_STRING = 4, TYPE_OPAQUE = 5
+} ValueType;
+
+/** A polymorphic value: int32, int64, real64, string or opaque. */
+typedef struct
+{
+  ValueType type;
+  
+  union
+  {
+    uint32_t  int32;
+    uint64_t  int64;
+    real64_t  real64;
+    char *    str;
+    uint8_t * bytes;
+  } value;
+} Value;
+
+/** 
+ * Initialise a polymorphic value.
+ *  
+ * @param value The value to init.
+ * @param type  The type of value.
+ * 
+ * The next param is the value to be assigned. For strings and opaques, this 
+ * value is "owned" by the poly value and will be free'd on calling
+ * value_free(). No type checking is done for this value.  
+ */
+Value *value_init (Value *value, ValueType type, ...);
+
+void value_free (Value *value);
+
+#define value_destroy(value) (value_free (value), free (value))
+
+/** Allocate and init an int32 value. Use value_destroy () when done. */
+#define value_create_int32(value) \
+  (value_init (malloc (sizeof (Value)), TYPE_INT32, value))
+
+#endif /*VALUES_H_*/
