1529df0a09a9f7654cac25b280a552790bfa56c9
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2008-05-24 00:05:57 +0000

    Initial revision.

---
+++ trunk/c/src/lib/log.c
@@ -0,0 +1,18 @@
+#include <elvin/log.h>
+
+#include <stdio.h>
+#include <stdarg.h>
+
+void elvin_log (int level, const char *message, ...)
+{
+  va_list args;
+  
+  va_start (args, message);
+  
+  // todo
+  fprintf (stderr, "log: ");
+  vfprintf (stderr, message, args);
+  fprintf (stderr, "\n");
+  
+  va_end (args);
+}
