2d85f5681eaeedd92b5a6c192e147c9f0bf7ae01
Matthew Phillips <mattphil@gmail.com>
2008-05-27 07:51:17 +0000

    Fixed for GNU/Unix.

--- c/src/include/elvin/stdtypes.h
+++ c/src/include/elvin/stdtypes.h
@@ -1,26 +1,34 @@
+/*
+ * Pulls in C99 stdint.h, stdbool.h, etc headers if available, or defines the 
+ * needed types if not. 
+ */
 #ifndef ELVIN_STDTYPES_H
 #define ELVIN_STDTYPES_H
 
-#ifdef HAVE_STDBOOL_H
-#include <stdbool.h>
-#else
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef signed long int64_t;
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long uint64_t;
+#ifdef HAVE_CONFIG_H
+  #include <config.h>
 #endif
 
-#ifdef HAVE_STDWINTBOOL_H
-#include <stdint.h>
+#ifdef HAVE_STDINT_H
+  #include <stdint.h>
 #else
-#define bool int
-#define true 1
-#define false 0
+  typedef signed char int8_t;
+  typedef signed short int16_t;
+  typedef signed int int32_t;
+  typedef signed long int64_t;
+
+  typedef unsigned char uint8_t;
+  typedef unsigned short uint16_t;
+  typedef unsigned int uint32_t;
+  typedef unsigned long uint64_t;
+#endif
+
+#ifdef HAVE_STDBOOL_H
+  #include <stdbool.h>
+#else
+  #define bool int
+  #define true 1
+  #define false 0
 #endif
 
 #endif //ELVIN_STDTYPES_H
