6a37c293b65a536e4e5f9b4b4b746123e0438ebc
Matthew Phillips <mattphil@gmail.com>
2008-05-29 05:33:56 +0000

    Added DIAGNOSTIC2.

--- c/src/include/elvin/log.h
+++ c/src/include/elvin/log.h
@@ -1,23 +1,27 @@
 #ifndef LOG_H_
 #define LOG_H_
 
-#define ELVIN_DEBUG_LEVEL 3
+#define ELVIN_LOG_LEVEL 1
 
 #define LOG_LEVEL_DIAGNOSTIC 5
 #define LOG_LEVEL_TRACE 6
 
-#if ELVIN_DEBUG_LEVEL >= LOG_LEVEL_TRACE
+#define LOGGING(loglevel) (ELVIN_LOG_LEVEL >= loglevel)
+
+#if LOGGING (LOG_LEVEL_TRACE)
   #define TRACE(message) (elvin_log (LOG_LEVEL_TRACE, message))
 #else
   #define TRACE(message)
 #endif
 
-#if ELVIN_DEBUG_LEVEL >= LOG_LEVEL_DIAGNOSTIC
+#if LOGGING (LOG_LEVEL_DIAGNOSTIC)
   #define DIAGNOSTIC(message) (elvin_log (LOG_LEVEL_DIAGNOSTIC, message)) 
   #define DIAGNOSTIC1(message, arg1) (elvin_log (LOG_LEVEL_DIAGNOSTIC, message, arg1))
+  #define DIAGNOSTIC2(message, arg1, arg2) (elvin_log (LOG_LEVEL_DIAGNOSTIC, message, arg1, arg2))
 #else
   #define DIAGNOSTIC(message) 
   #define DIAGNOSTIC1(message, arg1)
+  #define DIAGNOSTIC2(message, arg1, arg2)
 #endif
 
 void elvin_log (int level, const char *message, ...);
