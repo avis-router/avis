04f0ddbb95dd466a36be116ce2b78328a4a1e3dc
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2008-05-30 03:02:34 +0000

    Added proper EMPTY_NAMED_VALUES definition.

--- trunk/c/src/include/elvin/named_values.h
+++ trunk/c/src/include/elvin/named_values.h
@@ -5,9 +5,6 @@
 #include <elvin/errors.h>
 #include <elvin/values.h>
 
-/* TODO */
-#define EMPTY_NAMED_VALUES NULL
-
 struct hashtable;
 
 typedef struct 
@@ -15,11 +12,16 @@ typedef struct
   struct hashtable *table;
 } NamedValues;
 
+extern NamedValues _empty_named_values;
+
+/* TODO */
+#define EMPTY_NAMED_VALUES (&_empty_named_values)
+
 #define named_values_create() \
   (named_values_init (malloc (sizeof (NamedValues))))
 
 #define named_values_destroy(values) \
-  (named_values_free (values), free (values))
+  (named_values_free (values), free (values), values = NULL)
 
 NamedValues *named_values_init (NamedValues *values);
 
