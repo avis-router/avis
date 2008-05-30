9af7c7c37df193109a4ea15bcbbfd6c3ed2ce074
Matthew Phillips <mattphil@gmail.com>
2008-05-30 03:12:42 +0000

    Added notification.

--- c/src/test/basic_test.c
+++ c/src/test/basic_test.c
@@ -15,10 +15,28 @@ int main (int argc, const char * argv[])
     elvin_perror ("open", &error);
     exit (1);
   }
+
+  NamedValues *ntfn = named_values_create ();
+    
+  named_values_set_int32 (ntfn, "favourite number", 42);
+  named_values_set_string (ntfn, "some text", "paydirt");
+  
+  elvin_send (&elvin, ntfn, &error);
+
+  named_values_destroy (ntfn);
   
   elvin_close (&elvin);
   
-  printf ("success!\n");
-  
-  return 0;
+  if (elvin_error_ok (&error))
+  {
+    printf ("Success!\n");
+    
+    return 0;
+  } else
+  {
+    elvin_perror ("Elvin", &error);
+    
+    return 1;
+  }
 }
+
