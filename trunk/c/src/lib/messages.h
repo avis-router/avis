f5b6677f5987d284fb171d879c1e0608ed66460f
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2008-05-30 03:04:38 +0000

    Cleaned up message allocation code.

--- trunk/c/src/lib/messages.h
+++ trunk/c/src/lib/messages.h
@@ -21,7 +21,8 @@ typedef enum
   MESSAGE_ID_CONN_RQST = 49,
   MESSAGE_ID_CONN_RPLY = 50,
   MESSAGE_ID_DISCONN_RQST = 51,
-  MESSAGE_ID_DISCONN_RPLY = 52
+  MESSAGE_ID_DISCONN_RPLY = 52,
+  MESSAGE_ID_NOTIFY_EMIT = 56
 } MessageTypeID;
 
 typedef uint8_t * Message;
@@ -30,14 +31,14 @@ Message message_read (ByteBuffer *buffer, ElvinError *error);
 
 bool message_write (ByteBuffer *buffer, Message message, ElvinError *error);
 
-void message_destroy (Message message);
-
 #define message_alloca() ((Message)alloca (MAX_MESSAGE_SIZE))
+
+#define message_destroy(message) \
+  (message_free (message), free (message), message = NULL)
 
 Message message_init (Message message, MessageTypeID type, ...);
 
-/** Destroy and free () message */
-#define message_free(message) (message_destroy (message), free (message))
+void message_free (Message message);
 
 /** The message's type ID. */
 #define message_type_of(message) (*(uint32_t *)(message))
