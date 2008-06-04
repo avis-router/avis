db0df1fb7912124ba4804d801084aab767c33569
Matthew Phillips <mattphil@gmail.com>
2008-06-04 06:39:55 +0000

    Split array list into public/private.

--- c/src/test/check_array_list.c
+++ c/src/test/check_array_list.c
@@ -3,8 +3,9 @@
 #include <check.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-#include "array_list.h"
+#include "array_list_private.h"
 #include "check_ext.h"
 
 TCase *array_list_tests ();
@@ -37,6 +38,19 @@ START_TEST (test_array_list)
   
   fail_unless (array_list_size (list) == 0, "Not empty");
       
+  array_list_destroy (list);
+  
+  list = array_list_create (int, 5);
+  
+  array_list_add_ptr (list, "1");
+  array_list_add_ptr (list, "2");
+  array_list_add_ptr (list, "3");
+  array_list_add_ptr (list, "foobar");
+  
+  fail_unless (strcmp (array_list_get_ptr (list, 0), "1") == 0, "Not equal");
+  fail_unless (strcmp (array_list_get_ptr (list, 3), "foobar") == 0, 
+               "Not equal");
+
   array_list_destroy (list);
 }
 END_TEST
