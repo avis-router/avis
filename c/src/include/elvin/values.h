92f2843dcbec7a326377b06485b6d97f763bfd1f
Matthew Phillips <mattphil@gmail.com>
2008-06-05 03:06:51 +0000

    Updated docs.

--- c/src/include/elvin/values.h
+++ c/src/include/elvin/values.h
@@ -15,9 +15,15 @@ typedef enum
   TYPE_STRING = 4, TYPE_OPAQUE = 5
 } ValueType;
 
+/**
+ * A generic homogeneous, fixed length array of any type of item.
+ */
 typedef struct
 {
-  void *items;
+  /** A pointer to the items in the array. */
+  void * items;
+  
+  /** The number of items in the array. */
   size_t item_count; 
 } Array;
 
@@ -27,8 +33,10 @@ typedef struct
  * */
 typedef struct
 {
+  /** The type of value. */
   ValueType type;
   
+  /** The actual wrapped value (discriminated by type). */
   union
   {
     int32_t   int32;
@@ -39,12 +47,6 @@ typedef struct
   } value;
 } Value;
 
-#define array_create(item_type, item_count) \
-  (array_init (malloc (sizeof (Array)), item_count, sizeof (item_type)))
-
-#define array_destroy(value) \
-  (array_free (value), free (value), value = NULL)
-
 /** 
  * Initialise a polymorphic value.
  *  
@@ -53,7 +55,9 @@ typedef struct
  * 
  * The next param is the value to be assigned. For strings and opaques, this 
  * value is "owned" by the poly value and will be free'd on calling
- * value_free(). No type checking is done for this value.  
+ * value_free(), so constant strings passed in here should be strdup()'d 
+ * before being used in a value that will be value_free()'d.
+ * No type checking is done for the value parameter.  
  */
 Value *value_init (Value *value, ValueType type, ...);
 
@@ -61,13 +65,6 @@ Value *value_init (Value *value, ValueType type, ...);
  * Free any resources held by a value instance.
  */
 void value_free (Value *value);
-
-Array *array_init (Array *array, unsigned item_count, size_t item_length);
-
-void array_free (Array *array);
-
-#define array_get(array, index, item_type) \
-  (((item_type *)array->items) [index])
 
 /**
  * Destroy (free and NULL) a value instance.
@@ -79,8 +76,54 @@ void array_free (Array *array);
 #define value_create_int32(value) \
   (value_init (malloc (sizeof (Value)), TYPE_INT32, value))
 
-/** Allocate and init a string value. Use value_destroy() when done. */
+/** 
+ * Allocate and init a string value. Use value_destroy() when done. The
+ * string is duplicated before being added.
+ * 
+ * @see value_init()
+ */
 #define value_create_string(value) \
   (value_init (malloc (sizeof (Value)), TYPE_STRING, strdup (value)))
+
+/**
+ * Initialise an array.
+ * 
+ * @param array The array to initialise.
+ * @param The initial item count.
+ * @param The length of an item.
+ * 
+ * @see array_create()
+ */
+Array *array_init (Array *array, size_t item_count, size_t item_length);
+
+/**
+ * Free resources held by an array.
+ */
+void array_free (Array *array);
+
+/** 
+ * Create a new array instance on the heap.
+ * 
+ * @see array_destroy()
+ * @see array_init()
+ */
+#define array_create(item_type, item_count) \
+  (array_init (malloc (sizeof (Array)), item_count, sizeof (item_type)))
+
+/**
+ * Destroy an array created with array_create().
+ */
+#define array_destroy(value) \
+  (array_free (value), free (value), value = NULL)
+
+/**
+ * Get an item of a given type.
+ * 
+ * @param array The array.
+ * @param index The index of the item.
+ * @param item_type The type of items in the array.
+ */
+#define array_get(array, index, item_type) \
+  (((item_type *)array->items) [index])
 
 #endif /*VALUES_H_*/
