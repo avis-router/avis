6174e14d3e6ba332b66d32472c2f4425d0132a96
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2008-06-12 05:25:24 +0000

    Various fixes for Linux. Change -std=c89 to -std=gnu89.

--- trunk/c/src/include/avis/attributes.h
+++ trunk/c/src/include/avis/attributes.h
@@ -1,8 +1,8 @@
 /** \file
  * Elvin attributes (name/value pairs) support.
  */
-#ifndef ELVIN_NAMED_VALUES_H
-#define ELVIN_NAMED_VALUES_H
+#ifndef ELVIN_ATTRIBUTES_H
+#define ELVIN_ATTRIBUTES_H
 
 #include <avis/stdtypes.h>
 #include <avis/errors.h>
@@ -249,4 +249,4 @@ const char *attributes_get_string (Attributes *attributes, const char *name);
  */
 Array *attributes_get_opaque (Attributes *attributes, const char *name);
 
-#endif /* ELVIN_NAMED_VALUES_H */
+#endif /* ELVIN_ATTRIBUTES_H */
