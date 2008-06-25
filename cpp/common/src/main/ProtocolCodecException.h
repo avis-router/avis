0964ef195e443430ab3e9a1b1a8ba332a268e4dc
Matthew Phillips <mattphil@gmail.com>
2008-06-25 23:14:20 +0000

    Removed C++ client and support: archived in branches/sandbox/cpp_client.

--- cpp/common/src/main/ProtocolCodecException.h
+++
@@ -1,8 +0,0 @@
-#include <stdexcept>
-#include <string>
-
-class ProtocolCodecException : public std::runtime_error
-{
-public:
-	ProtocolCodecException(const char *message) : runtime_error(message) {};
-};
