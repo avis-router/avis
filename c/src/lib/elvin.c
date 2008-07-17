b4da6dc842b2ae9bde6269bd7caca765f718e3df
Matthew Phillips <mattphil@gmail.com>
2008-07-17 06:31:19 +0000

    Moved socket code to net.c.

--- c/src/lib/elvin.c
+++ c/src/lib/elvin.c
@@ -22,11 +22,8 @@
 #include <assert.h>
 #include <time.h>
 
-#ifdef WIN32
-  #define snprintf _snprintf
-#else
+#ifndef WIN32
   #include <unistd.h>
-  #include <sys/types.h>
 #endif
 
 #include <avis/stdtypes.h>
@@ -43,9 +40,6 @@
 
 static void elvin_shutdown (Elvin *elvin, CloseReason reason,
                             const char *message);
-
-static socket_t open_socket (const char *host, uint16_t port,
-                             ElvinError *error);
 
 static bool send_and_receive (Elvin *elvin, Message request,
                               Message reply, MessageTypeID reply_type,
@@ -65,10 +59,6 @@ static void deliver_notification (Elvin *elvin, Array *ids,
 static Subscription *elvin_subscription_init (Subscription *subscription);
 
 static void elvin_subscription_free (Subscription *subscription);
-
-#ifdef WIN32
-  static void init_windows_sockets (ElvinError *error);
-#endif
 
 bool elvin_open (Elvin *elvin, const char *router_uri, ElvinError *error)
 {
@@ -199,10 +189,6 @@ void elvin_shutdown (Elvin *elvin, CloseReason reason, const char *message)
 
   listeners_free (&elvin->close_listeners);
   listeners_free (&elvin->notification_listeners);
-
-  #ifdef WIN32
-    WSACleanup ();
-  #endif
 }
 
 bool elvin_event_loop (Elvin *elvin, ElvinError *error)
@@ -583,77 +569,6 @@ Subscription *subscription_with_id (Elvin *elvin, uint64_t id)
   return NULL;
 }
 
-socket_t open_socket (const char *host, uint16_t port, ElvinError *error)
-{
-  struct addrinfo hints;
-  struct addrinfo *info;
-  struct addrinfo *i;
-  int error_code;
-  socket_t sock = -1;
-  char service [10];
-
-  #ifdef WIN32
-    if (!init_windows_sockets (error))
-      return -1;
-  #endif
-
-  snprintf (service, sizeof (service), "%u", port);
-
-  memset (&hints, 0, sizeof (hints));
-  hints.ai_family = PF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-
-  if ((error_code = getaddrinfo (host, service, &hints, &info)))
-  {
-    elvin_error_set (error, host_to_elvin_error (error_code),
-                     gai_strerror (error_code));
-
-    return -1;
-  }
-
-  for (i = info; i && sock == -1; i = i->ai_next)
-  {
-    sock = socket (i->ai_family, SOCK_STREAM, 0);
-
-    if (sock != -1)
-    {
-      if (connect (sock, i->ai_addr, i->ai_addrlen) == 0)
-      {
-        /* set send/receive timeouts */
-        #ifdef WIN32
-          /*
-           * Windows seems to treat the seconds field as
-           * milliseconds from what I can see running tests.
-           * Lord only knows what it thinks the microseconds
-           * field is.
-           */
-          struct timeval timeout = {AVIS_IO_TIMEOUT, 0};
-        #else
-          struct timeval timeout =
-            {AVIS_IO_TIMEOUT / 1000, (AVIS_IO_TIMEOUT % 1000) * 1000};
-        #endif
-
-        setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO,
-                    (char *)&timeout, sizeof (timeout));
-        setsockopt (sock, SOL_SOCKET, SO_SNDTIMEO,
-                    (char *)&timeout, sizeof (timeout));
-      } else
-      {
-        close_socket (sock);
-
-        sock = -1;
-      }
-    }
-  }
-
-  freeaddrinfo (info);
-
-  if (sock == -1)
-    elvin_error_from_errno (error);
-
-  return sock;
-}
-
 void elvin_subscription_add_listener (Subscription *subscription,
                                       SubscriptionListener listener,
                                       void *user_data)
@@ -690,28 +605,3 @@ bool elvin_remove_notification_listener (Elvin *elvin,
 {
   return listeners_remove (&elvin->notification_listeners, (Listener)listener);
 }
-
-#ifdef WIN32
-
-void init_windows_sockets (ElvinError *error)
-{
-  WSADATA wsaData;
-  int err;
-
-  err = WSAStartup (MAKEWORD (2, 2), &wsaData);
-
-  if (err != 0)
-  {
-    elvin_error_set (error, ELVIN_ERROR_INTERNAL,
-                     "Failed to init winsock library");
-  } else if (LOBYTE (wsaData.wVersion) != 2 ||
-             HIBYTE (wsaData.wVersion) != 2)
-  {
-    WSACleanup ();
-
-    elvin_error_set (error, ELVIN_ERROR_INTERNAL,
-                     "Failed to find winsock 2.2");
-  }
-}
-
-#endif
---
+++ c/src/lib/net.c
@@ -0,0 +1,115 @@
+/*
+ * net.c
+ *
+ *  Created on: Jul 17, 2008
+ *      Author: matt
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <avis/defs.h>
+#include <avis/net.h>
+
+#ifdef WIN32
+  #define snprintf _snprintf
+
+  static void init_windows_sockets (ElvinError *error);
+#endif
+
+socket_t open_socket (const char *host, uint16_t port, ElvinError *error)
+{
+  struct addrinfo hints;
+  struct addrinfo *info;
+  struct addrinfo *i;
+  int error_code;
+  socket_t sock = -1;
+  char service [10];
+
+  #ifdef WIN32
+    if (!init_windows_sockets (error))
+      return -1;
+  #endif
+
+  snprintf (service, sizeof (service), "%u", port);
+
+  memset (&hints, 0, sizeof (hints));
+  hints.ai_family = PF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((error_code = getaddrinfo (host, service, &hints, &info)))
+  {
+    elvin_error_set (error, host_to_elvin_error (error_code),
+                     gai_strerror (error_code));
+
+    return -1;
+  }
+
+  for (i = info; i && sock == -1; i = i->ai_next)
+  {
+    sock = socket (i->ai_family, SOCK_STREAM, 0);
+
+    if (sock != -1)
+    {
+      if (connect (sock, i->ai_addr, i->ai_addrlen) == 0)
+      {
+        /* set send/receive timeouts */
+        #ifdef WIN32
+          /*
+           * Windows seems to treat the seconds field as
+           * milliseconds from what I can see running tests.
+           * Lord only knows what it thinks the microseconds
+           * field is.
+           */
+          struct timeval timeout = {AVIS_IO_TIMEOUT, 0};
+        #else
+          struct timeval timeout =
+            {AVIS_IO_TIMEOUT / 1000, (AVIS_IO_TIMEOUT % 1000) * 1000};
+        #endif
+
+        setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO,
+                    (char *)&timeout, sizeof (timeout));
+        setsockopt (sock, SOL_SOCKET, SO_SNDTIMEO,
+                    (char *)&timeout, sizeof (timeout));
+      } else
+      {
+        close_socket (sock);
+
+        sock = -1;
+      }
+    }
+  }
+
+  freeaddrinfo (info);
+
+  if (sock == -1)
+    elvin_error_from_errno (error);
+
+  return sock;
+}
+
+#ifdef WIN32
+
+void init_windows_sockets (ElvinError *error)
+{
+  WSADATA wsaData;
+  int err;
+
+  err = WSAStartup (MAKEWORD (2, 2), &wsaData);
+
+  if (err != 0)
+  {
+    elvin_error_set (error, ELVIN_ERROR_INTERNAL,
+                     "Failed to init winsock library");
+  } else if (LOBYTE (wsaData.wVersion) != 2 ||
+             HIBYTE (wsaData.wVersion) != 2)
+  {
+    WSACleanup ();
+
+    elvin_error_set (error, ELVIN_ERROR_INTERNAL,
+                     "Failed to find winsock 2.2");
+  }
+}
+
+#endif
