dbfc65b400262686e195c0069e90b5d8f10b7e37
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2008-12-02 21:17:51 +0000

    Added full message text formatting to ticker display.

--- trunk/objc/examples/ticker/TickerController.m
+++ trunk/objc/examples/ticker/TickerController.m
@@ -3,24 +3,74 @@
 
 @implementation TickerController
 
+#define color(r, g, b) \
+  [NSColor colorWithCalibratedRed: (r)/255.0 green: (g)/255.0 \
+   blue: (b)/255.0 alpha: 1]
+
+static NSAttributedString *attributedString (NSString *string, 
+                                             NSDictionary *attrs)
+{
+  return 
+    [[[NSAttributedString alloc] initWithString: string attributes: attrs] 
+      autorelease];
+}
+
 - (void) handleNotify: (NSDictionary *) message
 {
   NSRange endRange;
-  NSString *messageText = 
-    [NSString stringWithFormat: @">>> %@: %@: %@\n",
-      [message objectForKey: @"Group"],
-      [message objectForKey: @"From"],
-      [message objectForKey: @"Message"]];
+  NSDateFormatter *dateFormatter = [[NSDateFormatter new] autorelease];
+  [dateFormatter setDateStyle: NSDateFormatterShortStyle];
+  [dateFormatter setTimeStyle: NSDateFormatterMediumStyle];  
+
+  // define reusable display attributes
+  NSDictionary *dateAttrs = 
+   [NSDictionary dictionaryWithObject: color (102, 102, 102) 
+                               forKey: NSForegroundColorAttributeName];
+
+  NSDictionary *fromAttrs = 
+    [NSDictionary dictionaryWithObject: color (51, 51, 51) 
+                                forKey: NSForegroundColorAttributeName];
+
+  NSDictionary *messageAttrs = 
+   [NSDictionary dictionaryWithObject: color (0, 0, 128) 
+                               forKey: NSForegroundColorAttributeName];
   
+  // build formatted message
+  NSMutableAttributedString *displayedMessage = 
+    [[NSMutableAttributedString new] autorelease]; 
+
+  [displayedMessage appendAttributedString: 
+    attributedString ([dateFormatter stringFromDate: [NSDate date]], 
+                      dateAttrs)];
+  
+  [displayedMessage appendAttributedString: attributedString (@": ", dateAttrs)];
+  
+  [displayedMessage appendAttributedString: 
+    attributedString ([message objectForKey: @"Group"], fromAttrs)];
+  
+  [displayedMessage appendAttributedString: attributedString (@": ", fromAttrs)];
+  
+  [displayedMessage appendAttributedString: 
+    attributedString ([message objectForKey: @"From"], fromAttrs)];
+  
+  [displayedMessage appendAttributedString: attributedString (@": ", fromAttrs)];
+  
+  [displayedMessage appendAttributedString: 
+    attributedString ([message objectForKey: @"Message"], messageAttrs)];
+  
+  [displayedMessage 
+    appendAttributedString: attributedString (@"\n", messageAttrs)];
+  
+  // insert text
   endRange.location = [[text textStorage] length];
   endRange.length = 0;
   
-  [text replaceCharactersInRange: endRange withString: messageText];
+  [[text textStorage] replaceCharactersInRange: endRange
+                          withAttributedString: displayedMessage];
 
+  // scroll to end
   endRange.location = [[text textStorage] length];
   [text scrollRangeToVisible: endRange];
-  
-  [message release];
 }
 
 - (void) awakeFromNib
