09992644c065232c06ba876c3b88c81d44ae7baa
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-01-30 02:22:47 +0000

    Added initial presence groups preference.

--- trunk/objc/examples/ticker/PreferencesController.m
+++ trunk/objc/examples/ticker/PreferencesController.m
@@ -10,4 +10,36 @@
     return nil;
 }
 
+- (void)didEndSheet: (NSWindow *) sheet returnCode: (int) returnCode 
+        contextInfo: (void *) contextInfo
+{
+  NSLog (@"Code = %u", returnCode);
+  
+  //if (returnCode == NSOKButton)
+//  {
+//    NSArray *
+//  }
+  
+  [sheet orderOut: self];
+}
+
+- (IBAction) closeAddPresenceGroupSheet: (id) sender
+{
+  [NSApp endSheet: addPresenceGroupSheet
+       returnCode: (sender == addPresenceGroupAddButton ? 
+                      NSOKButton : NSCancelButton)];
+}
+
+- (IBAction) addPresenceGroup: (id) sender
+{
+  if (!addPresenceGroupSheet)
+    [NSBundle loadNibNamed: @"Preferences_AddPresenceGroup" owner: self];
+  
+  [NSApp beginSheet: addPresenceGroupSheet
+     modalForWindow: [self window]
+      modalDelegate: self
+     didEndSelector: @selector (didEndSheet:returnCode:contextInfo:)
+        contextInfo: nil];
+}
+
 @end
