b24940f2fdb090ee8a6ec6da6f4089a87cdb684f
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-03-30 00:48:46 +0000

    Initial key IO test running.

--- trunk/objc/examples/ticker/src/tests/TestKeyRegistry.m
+++ trunk/objc/examples/ticker/src/tests/TestKeyRegistry.m
@@ -6,12 +6,19 @@
 
 - (void) testReadKey
 {
+  NSString *resourceDir = 
+    [NSString stringWithCString: getenv ("TEST_RESOURCES_DIR") 
+                       encoding: NSASCIIStringEncoding];
+
   //STFail (@"Failed!");
   //KeyRegistry *registry = [KeyRegistry registryWithKeysInDir: @"test_keys"];
   //KeyRegistry *registry = [[KeyRegistry alloc] init];
   
   NSError *error;
-  Key *key = [[Key alloc] initWithFile: @"test_keys/key1.key" error: &error];
+  Key *key = 
+    [[Key alloc] 
+       initWithFile: [NSString stringWithFormat: @"%@/test_keys/key1.key", resourceDir] 
+       error: &error];
 
   STAssertNotNil (key, @"Error reading key: %@", [error localizedDescription]);
 }
