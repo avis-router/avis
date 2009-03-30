e3ec3caa2c9a72a2e9156342958dd7c0c7835d54
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-03-30 04:23:47 +0000

    Completed initial implemention of key IO.

--- trunk/objc/examples/ticker/src/tests/TestKeyRegistry.m
+++ trunk/objc/examples/ticker/src/tests/TestKeyRegistry.m
@@ -14,13 +14,70 @@
   //KeyRegistry *registry = [KeyRegistry registryWithKeysInDir: @"test_keys"];
   //KeyRegistry *registry = [[KeyRegistry alloc] init];
   
-  NSError *error;
-  Key *key = 
-    [[Key alloc] 
-       initWithFile: [NSString stringWithFormat: @"%@/test_keys/key1.key", resourceDir] 
-       error: &error];
+  NSError *error = nil;
+  Key *key1 = 
+    [[[Key alloc] 
+       initWithFile: 
+         [NSString stringWithFormat: @"%@/test_keys/key1.key", resourceDir] 
+       error: &error] autorelease];
 
-  STAssertNotNil (key, @"Error reading key: %@", [error localizedDescription]);
+  unsigned char key_data [16] = 
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+  
+  STAssertNotNil (key1, @"Error reading key: %@", [error localizedDescription]);
+  STAssertEqualObjects (@"test key #1", key1.name, @"Names not equal");
+  STAssertEquals (KEY_TYPE_PUBLIC, key1.type, @"Types not equal");
+  STAssertEqualObjects 
+    ([NSData dataWithBytes: key_data length: sizeof (key_data)], key1.data, 
+     @"Data not equal");
+  
+  error = nil;
+  
+  Key *key2 = 
+    [[[Key alloc] 
+      initWithFile: 
+      [NSString stringWithFormat: @"%@/test_keys/key_error_version.key", resourceDir] 
+        error: &error] autorelease];
+  
+  STAssertNil (key2, @"Key must be nil");
+  STAssertNotNil (error, @"Error must be set");
+  STAssertEquals (KEY_IO_VERSION, [error code], @"Wrong error code");
+  
+  error = nil;
+  
+  Key *key3 = 
+    [[[Key alloc] 
+      initWithFile: 
+        [NSString stringWithFormat: @"%@/test_keys/key_error_hexdata.key", resourceDir] 
+        error: &error] autorelease];
+  
+  STAssertNil (key3, @"Key must be nil");
+  STAssertNotNil (error, @"Error must be set");
+  STAssertEquals (KEY_IO_BAD_HEX_DIGIT, [error code], @"Wrong error code");
+  
+  error = nil;
+  
+  Key *key4 = 
+  [[[Key alloc] 
+    initWithFile: 
+    [NSString stringWithFormat: @"%@/test_keys/key_error_version.key", resourceDir] 
+    error: &error] autorelease];
+  
+  STAssertNil (key4, @"Key must be nil");
+  STAssertNotNil (error, @"Error must be set");
+  STAssertEquals (KEY_IO_VERSION, [error code], @"Wrong error code");
+  
+  error = nil;
+  
+  Key *key5 = 
+    [[[Key alloc] 
+      initWithFile: 
+      [NSString stringWithFormat: @"%@/test_keys/key2.key", resourceDir] 
+      error: &error] autorelease];
+  
+  STAssertNotNil (key5, @"Error reading key: %@", [error localizedDescription]);
+  STAssertEqualObjects (@"test key #2", key5.name, @"Names not equal");
+  STAssertEquals (KEY_TYPE_PRIVATE, key5.type, @"Types not equal");
 }
 
 @end
