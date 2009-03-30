43956657455e154c0541d0190f291e71bc27013b
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-03-30 21:54:55 +0000

    Added test for missing fields.

--- trunk/objc/examples/ticker/src/main/Key.h
+++ trunk/objc/examples/ticker/src/main/Key.h
@@ -3,7 +3,8 @@
 typedef enum {KEY_TYPE_PUBLIC, KEY_TYPE_PRIVATE} KeyAccessType;
 
 typedef enum {KEY_IO_VERSION, KEY_IO_ACCESS, KEY_IO_UNKNOWN_FIELD, 
-              KEY_IO_MISSING_VALUE, KEY_IO_BAD_HEX_DIGIT} KeyIOError;
+              KEY_IO_MISSING_VALUE, KEY_IO_BAD_HEX_DIGIT, 
+              KEY_IO_MISSING_FIELD} KeyIOError;
 
 @interface Key : NSObject 
 {
