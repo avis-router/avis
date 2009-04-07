00af534c5fa0b7b584df19919a0e09f99a332fe8
Matthew Phillips <mattphil@gmail.com>
2009-04-07 05:44:10 +0000

    Added key import from file.

---
+++ objc/examples/ticker/src/main/ElvinKeyIO.h
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+
+extern NSString *KeyFieldName;
+extern NSString *KeyFieldIsPrivate;
+extern NSString *KeyFieldData;
+
+typedef enum {KEY_IO_VERSION, KEY_IO_ACCESS, KEY_IO_UNKNOWN_FIELD, 
+              KEY_IO_MISSING_VALUE, KEY_IO_BAD_HEX_DATA, 
+              KEY_IO_MISSING_FIELD} KeyIOError;
+
+@interface ElvinKeyIO : NSObject
+{
+}
+
++ (NSDictionary *) keyFromFile: (NSString *) file error: (NSError **) error;
+
++ (NSDictionary *) keyFromString: (NSString *) contents error: (NSError **) error;
+
+@end
