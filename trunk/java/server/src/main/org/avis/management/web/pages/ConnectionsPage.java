e8f136aaf8ecb5456540dd017bbe1066c21bc0b5
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-06-30 06:22:44 +0000

    Added initial subscriptions dump.

--- trunk/java/server/src/main/org/avis/management/web/pages/ConnectionsPage.java
+++ trunk/java/server/src/main/org/avis/management/web/pages/ConnectionsPage.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.avis.management.web.HTML;
 import org.avis.router.Connection;
 import org.avis.router.Router;
+import org.avis.router.Subscription;
 
 import static java.text.DateFormat.SHORT;
 import static java.text.DateFormat.getDateTimeInstance;
@@ -38,23 +39,66 @@ public class ConnectionsPage extends Page
     for (Connection connection : router.connections ())
     {
       // TODO implement ${} option
-      html.append 
-        ("<tr><td rowspan=\"2\">${1} (${2})</td><td>${3}</td>\n" + 
-         "    <td>${4}</td><td>${5} / ${6}</td><td>${7}</td></tr>",
-         connection.serial, connection.id (),
-         formatConnectionTime (connection.connectedAt), 
-         connection.hostname (),
-         connection.sentNotificationCount, connection.receivedNotificationCount, 
-         connection.subscriptions.size ());
+      // TODO add relative connection time
+      connection.lockRead ();
+      
+      try
+      {
+        if (!connection.isOpen ())
+          continue;
+        
+        html.append 
+          ("<tr><td rowspan=\"2\">${1} (${2})</td><td>${3}</td>\n" + 
+           "    <td>${4}</td><td>${5} / ${6}</td><td>${7}</td></tr>",
+           connection.serial, connection.id (),
+           formatConnectionTime (connection.connectedAt), 
+           connection.hostname (),
+           connection.sentNotificationCount, connection.receivedNotificationCount, 
+           connection.subscriptions.size ());
+      
+        html.append ("<tr><td colspan=\"4\">\n");
+  
+        html.indent ();
+        outputSubscriptions (html, connection);
+        html.outdent ();
+        
+        html.append ("</td></tr>\n");
+      } finally
+      {
+        connection.unlockRead ();
+      }
     }
 
     html.outdent ();
-    
     html.append ("</table>");
     
     html.appendClosingTags ();
     
     return html.asText ();
+  }
+
+  private void outputSubscriptions (HTML html, Connection connection)
+  {
+    html.append ("<table>\n");
+
+    html.indent ();
+   
+    for (Subscription subscription : connection.subscriptions ())
+    {
+      html.append 
+        ("<tr><td>${1}</td><td class=\"sub-exp\">${2}</td><td>(${3})</td></tr>",
+         subscription.id, subscription.expr, guessSubType (subscription.expr));
+    }
+    
+    html.outdent ();
+    
+    html.append ("</table>\n");
+  }
+
+  private String guessSubType (String subExpr)
+  {
+    // TODO implement
+    return "Unknown";
   }
 
   private String formatConnectionTime (long time)
