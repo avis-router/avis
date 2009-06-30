7ce2b4edc73c93df31fbc820b5fadf5801e5733a
Matthew Phillips <mattphil@gmail.com>
2009-06-30 05:55:41 +0000

    First client info display implemented.

--- java/server/src/main/org/avis/management/web/pages/ConnectionsPage.java
+++ java/server/src/main/org/avis/management/web/pages/ConnectionsPage.java
@@ -1,7 +1,13 @@
 package org.avis.management.web.pages;
 
+import java.util.Date;
+
 import org.avis.management.web.HTML;
+import org.avis.router.Connection;
 import org.avis.router.Router;
+
+import static java.text.DateFormat.SHORT;
+import static java.text.DateFormat.getDateTimeInstance;
 
 public class ConnectionsPage extends Page
 {
@@ -25,12 +31,34 @@ public class ConnectionsPage extends Page
     html.append 
       ("<table class=\"client-list\" border=\"1\" cellspacing=\"0\">\n\n" +
        "  <tr><th>Client</th> <th>Connected</th> <th>Host</th> \n" + 
-       "      <th>Notifications</th> <th>Subscriptions</th></tr>\n");
+       "      <th>Notifications<br/>(Sent / Received)</th> <th>Subscriptions</th></tr>\n");
+    
+    html.indent ();
+    
+    for (Connection connection : router.connections ())
+    {
+      // TODO implement ${} option
+      html.append 
+        ("<tr><td rowspan=\"2\">${1} (${2})</td><td>${3}</td>\n" + 
+         "    <td>${4}</td><td>${5} / ${6}</td><td>${7}</td></tr>",
+         connection.serial, connection.id (),
+         formatConnectionTime (connection.connectedAt), 
+         connection.hostname (),
+         connection.sentNotificationCount, connection.receivedNotificationCount, 
+         connection.subscriptions.size ());
+    }
+
+    html.outdent ();
     
     html.append ("</table>");
     
     html.appendClosingTags ();
     
     return html.asText ();
+  }
+
+  private String formatConnectionTime (long time)
+  {
+    return getDateTimeInstance (SHORT, SHORT).format (new Date (time));
   }
 }
