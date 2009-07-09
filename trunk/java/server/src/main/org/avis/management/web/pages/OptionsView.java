2ff1a90d433c133ba07c72159fdecdd32c6dd376
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-07-09 05:06:56 +0000

    Added options for federation.

---
+++ trunk/java/server/src/main/org/avis/management/web/pages/OptionsView.java
@@ -0,0 +1,39 @@
+package org.avis.management.web.pages;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.avis.config.OptionType;
+import org.avis.config.Options;
+import org.avis.management.web.HTML;
+
+public class OptionsView implements HtmlView
+{
+  private Options options;
+  private Pattern filter;
+
+  public OptionsView (Options options, String filterRegex)
+  {
+    this.options = options;
+    this.filter = Pattern.compile (filterRegex);
+  }
+
+  public void render (HTML html)
+  {
+    html.append ("<p><div class='prop-list small-scrolling'><table>\n");
+    html.indent ();
+    
+    for (Map.Entry<String, OptionType> e : 
+         options.optionSet ().all ().entrySet ())
+    {
+      if (filter.matcher (e.getKey ()).matches ())
+      {
+        html.append ("<tr><td>${}:</td><td>${}</td></tr>\n",
+                     e.getKey (), options.get (e.getKey ()));
+      }
+    }
+    
+    html.outdent ();
+    html.append ("</table></div></p>\n");
+  }
+}
