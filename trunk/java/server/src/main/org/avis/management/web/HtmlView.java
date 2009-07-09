25eb5ac7031f6e637d5b328287485f4b9b1c6453
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-07-09 11:02:53 +0000

    Added docs.

--- trunk/java/server/src/main/org/avis/management/web/HtmlView.java
+++ trunk/java/server/src/main/org/avis/management/web/HtmlView.java
@@ -1,7 +1,14 @@
 package org.avis.management.web;
 
-
+/**
+ * An HTML view component.
+ * 
+ * @author Matthew Phillips
+ */
 public interface HtmlView
 {
+  /**
+   * Render this view onto the given HTML.
+   */
   public void render (HTML html);
 }
