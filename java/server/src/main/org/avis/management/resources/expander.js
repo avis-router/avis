30635ef945d5b2b1a3f079374dd3e9d7f25e52d4
Matthew Phillips <mattphil@gmail.com>
2009-08-05 02:56:08 +0000

    Tweakage.

--- java/server/src/main/org/avis/management/resources/expander.js
+++ java/server/src/main/org/avis/management/resources/expander.js
@@ -14,9 +14,9 @@ $(document).ready (function ()
     $(this).children ('.expander-ellipsis').toggle ();
 
     var body = $(this).next ('.expand-body');
-    var toggle = body.hasClass ('contracted') ? '- ' : '+ ';
+    var switcher = body.hasClass ('contracted') ? '- ' : '+ ';
 
     body.toggleClass ('contracted');
-    $(this).children ('.expander-toggle').text (toggle);
+    $(this).children ('.expander-toggle').text (switcher);
   })
 });
