efdbe94bed1b4e884ffba0b163e91940fbabcde7
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-08-17 02:49:18 +0000

    Added round-robin session enabling.

--- trunk/java/server/src/main/org/avis/router/LowMemoryThrottler.java
+++ trunk/java/server/src/main/org/avis/router/LowMemoryThrottler.java
@@ -1,18 +1,25 @@
 package org.avis.router;
 
+import java.util.Iterator;
+
 import org.apache.mina.core.filterchain.IoFilterAdapter;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.core.write.WriteRequest;
 
-import static java.lang.Thread.currentThread;
-
 import static java.lang.Runtime.getRuntime;
+import static java.lang.Thread.currentThread;
 
 public class LowMemoryThrottler extends IoFilterAdapter
 {
   private static final long HIGH_WATER = 4 * 1024 * 1024;
   private static final long LOW_WATER = 4 * 1024 * 1024;
   
+  /**
+   * While in low memory condition, loops though sessions periodically
+   * enabling one per second to allow draining to occur.
+   * 
+   * @author Matthew Phillips
+   */
   private final class PollingThread extends Thread
   {
     public PollingThread ()
@@ -25,14 +32,36 @@ public class LowMemoryThrottler extends IoFilterAdapter
     @Override
     public void run ()
     {
+      Iterator<IoSession> sessionIter = ioManager.sessions ().iterator ();
+      IoSession lastSession = null;
+      
       try
       {
-        // TODO think of more intelligent way to re-enable
+        // TODO think of more intelligent way to re-enable: use frame size
         while (getRuntime ().freeMemory () < LOW_WATER)
         {
-          System.out.println ("free = " + getRuntime ().freeMemory ());
+          IoSession session;
+          
+          if (lastSession != null)
+            lastSession.suspendRead ();
+          
+          if (!sessionIter.hasNext ())
+            sessionIter = ioManager.sessions ().iterator ();
+          
+          if (sessionIter.hasNext ())
+          {
+            session = sessionIter.next ();
+            session.resumeRead ();
+            lastSession = session;
+          } else
+          {
+            session = null;
+          }
+          
           System.gc ();
-        
+          System.out.println ("*** Poller: free = " + 
+                              getRuntime ().freeMemory ());
+
           synchronized (this)
           {
             wait (1000);
@@ -47,8 +76,8 @@ public class LowMemoryThrottler extends IoFilterAdapter
     }
   }
 
-  private IoManager ioManager;
-  private Thread poller;
+  protected IoManager ioManager;
+  protected Thread poller;
 
   public LowMemoryThrottler (IoManager ioManager)
   {
@@ -94,7 +123,8 @@ public class LowMemoryThrottler extends IoFilterAdapter
 
   protected void throttle ()
   {
-    System.out.println ("*** Throttle on Low memory");
+    System.out.println ("--- Throttle on Low memory " +
+                        getRuntime ().freeMemory ());
     
     // TODO suspend new connections
     for (IoSession session : ioManager.sessions ())
@@ -105,9 +135,8 @@ public class LowMemoryThrottler extends IoFilterAdapter
   {
     poller = null;
     
-    System.out.println ("*** Unthrottle");
+    System.out.println ("+++ Unthrottle " + getRuntime ().freeMemory ());
     
-    // TODO unsuspend new connections
     for (IoSession session : ioManager.sessions ())
       session.resumeRead ();
   }
@@ -128,6 +157,6 @@ public class LowMemoryThrottler extends IoFilterAdapter
       {
         currentThread ().interrupt ();
       }
-    }    
+    }
   }
 }
