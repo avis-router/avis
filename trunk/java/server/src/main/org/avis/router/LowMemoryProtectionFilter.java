4071fe04958884918a193b7654f7ddd202305667
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-08-25 04:04:55 +0000

    Now also implements Send-Queue.Receive-Length.

--- trunk/java/server/src/main/org/avis/router/LowMemoryProtectionFilter.java
+++ trunk/java/server/src/main/org/avis/router/LowMemoryProtectionFilter.java
@@ -1,73 +1,75 @@
 package org.avis.router;
 
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-
 import java.io.IOException;
 
-import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryUsage;
 
 import org.apache.mina.core.filterchain.IoFilterAdapter;
 import org.apache.mina.core.service.IoAcceptor;
 import org.apache.mina.core.session.IoSession;
+import org.apache.mina.core.write.DefaultWriteRequest;
 import org.apache.mina.core.write.WriteRequest;
 
-import org.avis.util.Collections;
+import org.avis.io.messages.DropWarn;
+import org.avis.io.messages.NotifyDeliver;
 
-import static java.lang.Math.max;
 import static java.lang.Runtime.getRuntime;
 import static java.lang.Thread.currentThread;
+import static java.lang.management.ManagementFactory.getMemoryMXBean;
 
-import static org.avis.common.Common.MB;
 import static org.avis.io.Net.idFor;
 import static org.avis.logging.Log.DIAGNOSTIC;
 import static org.avis.logging.Log.alarm;
 import static org.avis.logging.Log.diagnostic;
 import static org.avis.logging.Log.shouldLog;
 import static org.avis.logging.Log.warn;
+import static org.avis.router.RouterOptionSet.sendQueueDropPolicy;
+import static org.avis.router.RouterOptionSet.sendQueueMaxLength;
 import static org.avis.router.StatsFilter.readBytesThroughput;
 import static org.avis.router.StatsFilter.updateThroughput;
+import static org.avis.util.Text.formatNumber;
 
 /**
- * Checks free memory at every message send/receive, and triggers low
- * memory state protection when memory has dropped to a certain level.
+ * Checks free memory and send queue size at every message
+ * send/receive.
+ * 
+ * Triggers low memory state protection when memory has dropped to a
+ * certain level.
  * 
  * In low memory state:
  * <ul>
  * <li>Each time the low memory trigger is hit, the "spammiest" client
  * (if any) gets dropped.
  * <li>All reads from clients are suspended
- * <li>Client reads are resumed in round-robin for a second each until enough
- * memory is available.
+ * <li>Reads are resumed when enough memory is available.
  * </ul>
  * 
  * @author Matthew Phillips
  */
 public class LowMemoryProtectionFilter extends IoFilterAdapter
 {
+  private static final String IN_DROPWARN_STATE = "dropwarn";
+  
   protected IoManager ioManager;
   protected Thread poller;
-  protected long lowMemoryTrigger;
-  protected long lowMemoryUntrigger;
+  protected long lowMemoryThreshold;
 
-  public LowMemoryProtectionFilter (IoManager ioManager, int maxFrameSize)
+  public LowMemoryProtectionFilter (IoManager ioManager, 
+                                    long lowMemoryThreshold)
   {
     this.ioManager = ioManager;
-    this.lowMemoryTrigger = max (maxFrameSize * 2, 4*MB);
-    this.lowMemoryUntrigger = lowMemoryTrigger;
+    this.lowMemoryThreshold = lowMemoryThreshold;
     
     System.gc ();
     
-    if (freeMemory () < lowMemoryTrigger)
+    if (freeMemory () < lowMemoryThreshold)
     {
       warn 
         ("Low memory trigger has been reached before router has started: " +
          "disabling low memory crash protection. Consider raising the VM's " +
          "maximum memory allocation to avoid this.", this);
       
-      lowMemoryTrigger = 0;
+      lowMemoryThreshold = 0;
     }
   }
   
@@ -91,6 +93,46 @@ public class LowMemoryProtectionFilter extends IoFilterAdapter
   {
     checkForLowMemory ();
     
+    if (writeRequest.getMessage () instanceof NotifyDeliver && 
+        (inLowMemoryState () || 
+          session.getScheduledWriteBytes () > sendQueueMaxLength (session)))
+    {
+      if (session.getAttribute (IN_DROPWARN_STATE) == null)
+      {
+        String policy = sendQueueDropPolicy (session);
+        
+        warn 
+          ("Session " + idFor (session) + " has passed its maximum send " +
+           "queue size (Send-Queue.Max-Length) of " + 
+           formatNumber (sendQueueMaxLength (session)) + ": " + 
+           (policy.equals ("fail") ? "killing session" : "dropping packets"), 
+           this);
+        
+        if (policy.equals ("fail"))
+        {
+          session.close (true);
+        } else 
+        {
+          if (session.getAttribute (IN_DROPWARN_STATE) == null)
+          {
+            session.setAttribute (IN_DROPWARN_STATE);
+            
+            super.filterWrite (nextFilter, session, 
+                               new DefaultWriteRequest (new DropWarn ()));
+          }
+        }
+      }
+      
+      // drop NotifyDeliver
+      return;
+    } else if (session.getAttribute (IN_DROPWARN_STATE) != null)
+    {
+      session.removeAttribute (IN_DROPWARN_STATE);
+      
+      diagnostic 
+        ("Stopped dropping packets for session " + idFor (session), this);
+    }
+    
     super.filterWrite (nextFilter, session, writeRequest);
   }
   
@@ -106,19 +148,19 @@ public class LowMemoryProtectionFilter extends IoFilterAdapter
 
   private void checkForLowMemory ()
   {
-    if (!inLowMemoryState () && freeMemory () < lowMemoryTrigger)
-    {
+    if (!inLowMemoryState () && freeMemory () < lowMemoryThreshold)
+    {      
       synchronized (this)
       {
         System.gc ();
-
+        
         long freeMemory = freeMemory ();
         
-        if (freeMemory < lowMemoryTrigger && !inLowMemoryState ())
+        if (freeMemory < lowMemoryThreshold && !inLowMemoryState ())
         {
           warn ("Entering low memory state: throttling clients: " +
                 formatBytes (freeMemory) + " < " + 
-                formatBytes (lowMemoryTrigger) + " bytes free", this);        
+                formatBytes (lowMemoryThreshold) + " bytes free", this);        
   
           enterLowMemoryState ();
         }
@@ -205,8 +247,7 @@ public class LowMemoryProtectionFilter extends IoFilterAdapter
      * optimistic case (a memory alloc could still fail due to lack of
      * system RAM).
      */
-    MemoryUsage usage = 
-      ManagementFactory.getMemoryMXBean ().getHeapMemoryUsage ();
+    MemoryUsage usage = getMemoryMXBean ().getHeapMemoryUsage ();
     
     return usage.getMax () - usage.getUsed ();
   }
@@ -231,44 +272,27 @@ public class LowMemoryProtectionFilter extends IoFilterAdapter
     {
       throttle ();
       
-      SessionIterator sessionIter = new SessionIterator (ioManager);
-      IoSession lastSession = null;
       long lastGc = 0;
       
       try
       {
-        while (getRuntime ().freeMemory () < lowMemoryUntrigger)
+        while (getRuntime ().freeMemory () < lowMemoryThreshold)
         {
-          killASpammyClient ();
-          
-          if (lastSession != null)
-            lastSession.suspendRead ();
-          
-          if (sessionIter.hasNext ())
-          {
-            lastSession = sessionIter.next ();
-            lastSession.resumeRead ();
-            
-            System.out.println ("**** resumed " + lastSession.getId ());
-          } else
-          {
-            lastSession = null;
-          }
+          killBiggestClient ();
           
           if (System.currentTimeMillis () - lastGc > 2000)
           {
             lastGc = System.currentTimeMillis ();
 
-            System.out.println ("*** GC");
-            
             System.gc ();
           }
           
           if (shouldLog (DIAGNOSTIC))
           {
-            diagnostic ("Low memory manager: " + 
-                        formatBytes (getRuntime ().freeMemory ()) + 
-                        " free memory", this);
+            diagnostic 
+              ("Low memory manager: " + 
+               formatBytes (getRuntime ().freeMemory ()) + 
+               " free memory", this);
           }
           
           sleep (300);
@@ -280,18 +304,17 @@ public class LowMemoryProtectionFilter extends IoFilterAdapter
       {
         diagnostic ("Leaving low memory state: unthrottling clients: " +
                     formatBytes (getRuntime ().freeMemory ()) + " > " + 
-                    formatBytes (lowMemoryUntrigger) + " bytes free", this);
+                    formatBytes (lowMemoryThreshold) + " bytes free", this);
         
         unthrottle ();
       }
     }
 
     /**
-     * Kil the first client with more than 50Kb/s incoming.
+     * Kil the client using the most bandwidth.
      */
-    private void killASpammyClient ()
+    private void killBiggestClient ()
     {
-//      long maxThrougput = 50 * 1024;
       long maxThrougput = 0;
       IoSession spammyClient = null;
       
@@ -317,83 +340,5 @@ public class LowMemoryProtectionFilter extends IoFilterAdapter
         spammyClient.close (true);
       }
     }
-  }
-  
-  private static class SessionIterator implements Iterator<IoSession>
-  {
-    private IoManager ioManager;
-    private IoSession currentSession;
-
-    public SessionIterator (IoManager ioManager)
-    {
-      this.ioManager = ioManager;
-      this.currentSession = null;
-      
-      advance ();
-    }
-
-    private void advance ()
-    {
-      List<IoSession> sessions = sort (ioManager.sessions ());
-      
-      long currentSessionId = 
-        currentSession == null ? 0 : currentSession.getId () + 1;
-      
-      for (IoSession session : sessions)
-      {
-        if (!session.isClosing () && 
-            session.isConnected () && 
-            session.getId () >= currentSessionId)
-        {
-          currentSession = session;
-          break;
-        }
-      }
-      
-      if (currentSession == null && !sessions.isEmpty ())
-        currentSession = sessions.iterator ().next ();
-    }
-
-    private List<IoSession> sort (List<IoSession> sessions)
-    {
-      Collections.sort (sessions, new Comparator<IoSession> ()
-      {
-        public int compare (IoSession s1, IoSession s2)
-        {
-          long diff = s1.getId () - s2.getId ();
-          
-          if (diff < 0)
-            return -1;
-          else if (diff > 0)
-            return 1;
-          else
-            return 0;
-        }
-      });
-      
-      return sessions;
-    }
-
-    public boolean hasNext ()
-    {
-      return currentSession != null;
-    }
-
-    public IoSession next ()
-    {
-      if (currentSession == null)
-        throw new UnsupportedOperationException ();
-      
-      IoSession session = currentSession;
-      
-      advance ();
-      
-      return session;
-    }
-
-    public void remove ()
-    {
-      throw new UnsupportedOperationException ();
-    }
-  }
+  }  
 }
