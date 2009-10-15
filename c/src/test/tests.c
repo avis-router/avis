d790bfa11c2f4abb624660e7a39e0696ea2051ae
Matthew Phillips <mattphil@gmail.com>
2009-10-15 02:54:30 +0000

    Updated.

--- c/src/test/tests.c
+++ c/src/test/tests.c
@@ -22,6 +22,8 @@
 
 #include <check.h>
 
+Suite *suite ();
+
 TCase *messages_tests ();
 TCase *client_tests ();
 TCase *collections_tests ();
@@ -48,7 +50,10 @@ int main ()
   int number_failed;
   Suite *s = suite ();
   SRunner *sr = srunner_create (s);
-  srunner_set_fork_status (sr, CK_FORK);
+  
+  // Use env var CK_FORK=no to not fork
+  // srunner_set_fork_status (sr, CK_FORK);
+  
   srunner_run_all (sr, CK_NORMAL);
   number_failed = srunner_ntests_failed (sr);
   srunner_free (sr);
