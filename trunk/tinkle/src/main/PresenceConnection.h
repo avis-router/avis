8091b3e2a7251148aa0f11e93bccf57969dde630
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-11-16 05:59:56 +0000

    Presence now updates to match prefs.

--- trunk/tinkle/src/main/PresenceConnection.h
+++ trunk/tinkle/src/main/PresenceConnection.h
@@ -12,14 +12,26 @@ extern NSString *PresenceStatusChangedNotification;
   ElvinConnection    * elvin;
   NSMutableSet       * entities;
   PresenceStatus     * presenceStatus;
+  NSArray            * groups;
+  NSArray            * buddies;
+  NSString           * userId;
+  NSString           * userName;
   RHSystemIdleTimer  * idleTimer;
+  id                   presenceInfoSubscription;
+  id                   presenceRequestSubscription;
 }
 
-- (id) initWithElvin: (ElvinConnection *) theElvinConnection;
+- (id) initWithElvin: (ElvinConnection *) theElvinConnection 
+              userId: (NSString *) newUserId userName: (NSString *) newUserName
+              groups: (NSArray *) newGroups buddies: (NSArray *) newBuddies;
 
 - (void) refresh;
 
 @property (readonly, assign)  IBOutlet NSMutableSet   * entities;
 @property (readwrite, retain) IBOutlet PresenceStatus * presenceStatus;
+@property (readwrite, retain) NSArray                 * groups;
+@property (readwrite, retain) NSArray                 * buddies;
+@property (readonly, retain)  NSString                * userId;
+@property (readwrite, retain) NSString                * userName;
 
 @end
