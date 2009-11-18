a31d941a7d3853311aa83b1ad2030ae43e28dca5
phillipsm <phillipsm@63a85b2f-9b21-0410-bc4b-d65bfd1679b8>
2009-11-18 05:43:12 +0000

    Made show unread messages a preference.

--- trunk/tinkle/src/main/Preferences.m
+++ trunk/tinkle/src/main/Preferences.m
@@ -1,12 +1,13 @@
 #import "Preferences.h"
 
-NSString *PrefOnlineUserName        = @"OnlineUserName";
-NSString *PrefOnlineUserUUID        = @"OnlineUserUUID";
-NSString *PrefElvinURL              = @"ElvinURL";
-NSString *PrefDefaultSendGroup      = @"DefaultSendGroup";
-NSString *PrefTickerGroups          = @"TickerGroups";
-NSString *PrefPresenceGroups        = @"PresenceGroups";
-NSString *PrefPresenceBuddies       = @"PresenceBuddies";
-NSString *PrefTickerSubscription    = @"TickerSubscription";
-NSString *PrefPresenceColumnSorting = @"PresenceColumnSorting";
-NSString *PrefElvinKeys             = @"Keys";
+NSString *PrefOnlineUserName         = @"OnlineUserName";
+NSString *PrefOnlineUserUUID         = @"OnlineUserUUID";
+NSString *PrefElvinURL               = @"ElvinURL";
+NSString *PrefDefaultSendGroup       = @"DefaultSendGroup";
+NSString *PrefTickerGroups           = @"TickerGroups";
+NSString *PrefPresenceGroups         = @"PresenceGroups";
+NSString *PrefPresenceBuddies        = @"PresenceBuddies";
+NSString *PrefTickerSubscription     = @"TickerSubscription";
+NSString *PrefPresenceColumnSorting  = @"PresenceColumnSorting";
+NSString *PrefElvinKeys              = @"Keys";
+NSString *PrefShowUnreadMessageCount = @"ShowUnreadMessageCount";
