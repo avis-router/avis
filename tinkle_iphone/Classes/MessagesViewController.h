bf55c75d4ce5dcf58bad1b4c0dd5a72e64530cb4
Matthew Phillips <mattphil@gmail.com>
2009-11-22 02:41:02 +0000

    Very basic ticker messaging working.

--- tinkle_iphone/Classes/MessagesViewController.h
+++ tinkle_iphone/Classes/MessagesViewController.h
@@ -1,8 +1,25 @@
 #import <UIKit/UIKit.h>
 
+extern NSString *TickerMessageReceivedNotification;
+
+@class ElvinConnection;
+
 @interface MessagesViewController : UIViewController 
 {
-
+  ElvinConnection      *elvin;
+  NSString             *subscription;
+  id                    subscriptionContext;
+  BOOL                  canSend;
+  
+  IBOutlet UITextView  *messagesTextView;
+  IBOutlet UIButton    *sendButton;
+  IBOutlet UITextField *messageCompositionField;
 }
+
+@property (readwrite, retain) ElvinConnection   *elvin;
+@property (readwrite, retain) IBOutlet NSString *subscription;
+@property (readwrite, assign) IBOutlet BOOL      canSend;
+
+- (IBAction) sendMessage: (id) sender;
 
 @end
