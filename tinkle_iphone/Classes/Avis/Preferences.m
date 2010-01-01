76966d2b006e7999f5722a061d480efda81a9ba3
Matthew Phillips <mattphil@gmail.com>
2010-01-01 23:57:39 +0000

    Better default user name.

--- tinkle_iphone/Classes/Avis/Preferences.m
+++ tinkle_iphone/Classes/Avis/Preferences.m
@@ -13,6 +13,20 @@ NSString *PrefTickerSubscription    = @"TickerSubscription";
 NSString *PrefPresenceColumnSorting = @"PresenceColumnSorting";
 NSString *PrefElvinKeys             = @"Keys";
 
+NSString *defaultUserName ()
+{
+  NSRange iphonePostfix = 
+    [[[UIDevice currentDevice] name] rangeOfString: @"’s iPhone"];
+  NSString *name;
+  
+  if (iphonePostfix.location == NSNotFound)
+    name = @"Tinkle User";
+  else
+    name = [device substringToIndex: iphonePostfix.location];
+
+  return [NSString stringWithFormat: @"%@@iPhone", name];
+}
+
 void registerUserDefaults ()
 {
   NSUserDefaults *preferences = [NSUserDefaults standardUserDefaults];
@@ -23,11 +37,7 @@ void registerUserDefaults ()
     [preferences setObject: uuidString () forKey: PrefOnlineUserUUID];
 
   if (![preferences objectForKey: PrefOnlineUserName])
-  {
-    [defaults setObject: [NSString stringWithFormat: @"%@@%@", 
-                          @"Tinkle User", [[UIDevice currentDevice] name]] 
-      forKey: PrefOnlineUserName];
-  }
+    [defaults setObject: defaultUserName () forKey: PrefOnlineUserName];
   
   [defaults setObject: @"elvin://public.elvin.org" forKey: PrefElvinURL];
   [defaults setObject: @"Chat" forKey: PrefDefaultSendGroup];  
