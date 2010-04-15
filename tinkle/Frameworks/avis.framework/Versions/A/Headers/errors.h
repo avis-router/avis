5400a7a520d343a55e232fd7129ca8a8346f7dda
Matthew Phillips <mattphil@gmail.com>
2010-04-15 02:38:43 +0000

    Updated to latest avis.

--- tinkle/Frameworks/avis.framework/Versions/A/Headers/errors.h
+++ tinkle/Frameworks/avis.framework/Versions/A/Headers/errors.h
@@ -78,14 +78,10 @@ typedef struct
 #define ELVIN_ERROR_ROUTER_FAILURE      (ELVIN_ERROR_BASE + 10)
 
 AVIS_PUBLIC
-void *do_avis_emalloc (size_t size, const char *file, int line);
+void *avis_emalloc (size_t size);
 
 AVIS_PUBLIC
-char *do_avis_estrdup (const char *str, const char *file, int line);
-
-#define avis_emalloc(size) do_avis_emalloc ((size), __FILE__, __LINE__)
-
-#define avis_estrdup(str) do_avis_estrdup ((str), __FILE__, __LINE__)
+char *avis_estrdup (const char *str);
 
 /**
  * An empty error instance. This can be assigned to an error instance to
